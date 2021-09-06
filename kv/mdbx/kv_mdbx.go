e4f2dab9bd74181df5ca1d99504948a86a581dbd
alex.sharov <AskAlexSharov@gmail.com>
2021-09-06 17:28:56 +0700

    tx parse fuzz

--- kv/mdbx/kv_mdbx.go
+++ kv/mdbx/kv_mdbx.go
@@ -293,23 +293,24 @@ func (opts MdbxOpts) Open() (kv.RwDB, error) {
 		} else if staleReaders > 0 {
 			db.log.Info("[db] cleared reader slots from dead processes", "amount", staleReaders)
 		}
-	}
-	if opts.label == kv.ChainDB {
-		go func() {
-			for {
-				<-time.After(time.Second)
-				if err := db.View(context.Background(), func(tx kv.Tx) error {
-					info, err := tx.(*MdbxTx).tx.Info(false)
-					if err != nil {
+
+		if opts.label == kv.ChainDB {
+			go func() {
+				for {
+					<-time.After(time.Second)
+					if err := db.View(context.Background(), func(tx kv.Tx) error {
+						info, err := tx.(*MdbxTx).tx.Info(false)
+						if err != nil {
+							return err
+						}
+						fmt.Printf("id: %d\n", info.Id)
 						return err
+					}); err != nil {
+						panic(err)
 					}
-					fmt.Printf("id: %d\n", info.Id)
-					return err
-				}); err != nil {
-					panic(err)
 				}
-			}
-		}()
+			}()
+		}
 	}
 	return db, nil
 }
