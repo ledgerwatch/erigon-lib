b8f6af814943ebec319f4bb6198f4950f45b3ed2
alex.sharov <AskAlexSharov@gmail.com>
2021-09-09 19:01:05 +0700

    save

---
+++ kv/kvcache/cache_test.go
@@ -0,0 +1,139 @@
+package kvcache
+
+import (
+	"context"
+	"encoding/binary"
+	"fmt"
+	"testing"
+
+	"github.com/ledgerwatch/erigon-lib/gointerfaces"
+	"github.com/ledgerwatch/erigon-lib/gointerfaces/remote"
+	"github.com/ledgerwatch/erigon-lib/kv"
+	"github.com/ledgerwatch/erigon-lib/kv/memdb"
+	"github.com/stretchr/testify/require"
+)
+
+func bigEndian(n uint64) []byte {
+	num := [8]byte{}
+	binary.BigEndian.PutUint64(num[:], n)
+	return num[:]
+}
+
+func copyBytes(b []byte) (copiedBytes []byte) {
+	if b == nil {
+		return nil
+	}
+	copiedBytes = make([]byte, len(b))
+	copy(copiedBytes, b)
+	return
+}
+
+func TestAPI(t *testing.T) {
+	require := require.New(t)
+	c := New()
+	k1, k2 := [20]byte{1}, [20]byte{2}
+	db := memdb.NewTestDB(t)
+	get := func(key [20]byte) (res [1]chan []byte) {
+		for i := 0; i < len(res); i++ {
+			res[i] = make(chan []byte)
+			go func(out chan []byte) {
+				require.NoError(db.View(context.Background(), func(tx kv.Tx) error {
+					cache, err := c.View(tx)
+					if err != nil {
+						panic(err)
+					}
+					v, err := cache.Get(key[:], tx)
+					if err != nil {
+						panic(err)
+					}
+					fmt.Printf("a2: %x,%#v,%#v\n", key, v, copyBytes(v))
+					out <- copyBytes(v)
+					return nil
+				}))
+			}(res[i])
+		}
+		return res
+	}
+	put := func(blockNum uint64, blockHash [32]byte, k, v []byte) {
+		require.NoError(db.Update(context.Background(), func(tx kv.RwTx) error {
+			_ = tx.Put(kv.SyncStageProgress, []byte("Finish"), bigEndian(blockNum))
+			_ = tx.Put(kv.HeaderCanonical, bigEndian(blockNum), blockHash[:])
+			_ = tx.Put(kv.PlainState, k, v)
+			return nil
+		}))
+	}
+	// block 1 - represents existing state (no notifications about this data will come to client)
+	put(1, [32]byte{}, k2[:], []byte{42})
+
+	res1, res2 := get(k1), get(k2) // will return immediately
+	for i := range res1 {
+		require.Nil(<-res1[i])
+	}
+	for i := range res2 {
+		require.Equal([]byte{42}, <-res2[i])
+	}
+	put(2, [32]byte{}, k1[:], []byte{2})
+	fmt.Printf("-----\n")
+	res3, res4 := get(k1), get(k2)       // will see View of transaction 2
+	put(3, [32]byte{}, k1[:], []byte{3}) // even if core already on block 3
+
+	c.OnNewBlock(&remote.StateChange{
+		Direction:   remote.Direction_FORWARD,
+		BlockHeight: 2,
+		BlockHash:   gointerfaces.ConvertHashToH256([32]byte{}),
+		Changes: []*remote.AccountChange{{
+			Action:  remote.Action_UPSERT,
+			Address: gointerfaces.ConvertAddressToH160(k1),
+			Data:    []byte{1},
+		}},
+	})
+
+	for i := range res3 {
+		require.Equal([]byte{2}, <-res3[i])
+	}
+	for i := range res4 {
+		require.Equal([]byte{42}, <-res4[i])
+	}
+	fmt.Printf("-----\n")
+
+	res5, res6 := get(k1), get(k2) // will see View of transaction 3, even if notification has not enough changes
+	c.OnNewBlock(&remote.StateChange{
+		Direction:   remote.Direction_FORWARD,
+		BlockHeight: 3,
+		BlockHash:   gointerfaces.ConvertHashToH256([32]byte{}),
+		Changes: []*remote.AccountChange{{
+			Action:  remote.Action_UPSERT,
+			Address: gointerfaces.ConvertAddressToH160(k1),
+			Data:    []byte{3},
+		}},
+	})
+
+	for i := range res5 {
+		require.Equal([]byte{3}, <-res5[i])
+	}
+	for i := range res6 {
+		require.Equal([]byte{42}, <-res6[i])
+	}
+	fmt.Printf("-----\n")
+	put(2, [32]byte{2}, k1[:], []byte{2})
+	res7, res8 := get(k1), get(k2) // will see View of transaction 3, even if notification has not enough changes
+
+	c.OnNewBlock(&remote.StateChange{
+		Direction:   remote.Direction_UNWIND,
+		BlockHeight: 2,
+		BlockHash:   gointerfaces.ConvertHashToH256([32]byte{2}),
+		Changes: []*remote.AccountChange{{
+			Action:  remote.Action_UPSERT,
+			Address: gointerfaces.ConvertAddressToH160(k1),
+			Data:    []byte{2},
+		}},
+	})
+
+	for i := range res7 {
+		require.Equal([]byte{2}, <-res7[i])
+	}
+	for i := range res8 {
+		require.Equal([]byte{42}, <-res8[i])
+	}
+
+}
--- kv/kvcache/kvcache.go
+++ kv/kvcache/kvcache.go
@@ -1,13 +1,18 @@
 package kvcache
 
 import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
 	"sync"
 
 	_ "github.com/VictoriaMetrics/fastcache"
 	"github.com/google/btree"
 	_ "github.com/google/btree"
+	_ "github.com/iwanbk/bcache"
+	"github.com/ledgerwatch/erigon-lib/gointerfaces"
+	"github.com/ledgerwatch/erigon-lib/gointerfaces/remote"
 	"github.com/ledgerwatch/erigon-lib/kv"
-	"golang.org/x/sync/singleflight"
 	_ "golang.org/x/sync/singleflight"
 )
 
@@ -24,19 +29,179 @@ import (
 //      - otherwise - ???
 // - If found in cache - return value without copy (reader can rely on fact that data are immutable until end of db transaction)
 // - Otherwise just read from db (no requests deduplication for now - preliminary optimization).
+//
+// Pair.Value == nil - is a marker of absense key in db
 
-var roots map[string]CacheRoot
-var rootsLock sync.RWMutex
-
+type Cache struct {
+	latest    string //latest root
+	roots     map[string]*CacheRoot
+	rootsLock sync.RWMutex
+}
 type CacheRoot struct {
 	cache *btree.BTree
 	lock  sync.RWMutex
-	cond  *sync.Cond
+	ready chan struct{} // close when ready
 }
 
-var g singleflight.Group
-
-func A(key, kv kv.RoDB) {
-	g.Do("", key)
-	singleflight.Group{}
+type Pair struct {
+	K, V []byte
 }
+
+func (p *Pair) Less(than btree.Item) bool { return bytes.Compare(p.K, than.(*Pair).K) < 0 }
+
+func New() *Cache {
+	return &Cache{roots: map[string]*CacheRoot{}}
+}
+
+// selectOrCreateRoot - used for usual getting root
+func (c *Cache) selectOrCreateRoot(root string) *CacheRoot {
+	c.rootsLock.RLock()
+	r, ok := c.roots[root]
+	c.rootsLock.RUnlock()
+	if ok {
+		return r
+	}
+
+	c.rootsLock.Lock()
+	r = &CacheRoot{ready: make(chan struct{})}
+	latestRoot, ok := c.roots[c.latest]
+	if ok {
+		fmt.Printf("clone: %x\n", c.latest)
+		r.cache = latestRoot.cache.Clone()
+	} else {
+		fmt.Printf("create empty root: %x\n", root)
+		r.cache = btree.New(32)
+	}
+	c.roots[root] = r
+	c.rootsLock.Unlock()
+	return r
+}
+
+// advanceRoot - used for advancing root onNewBlock
+func (c *Cache) advanceRoot(root string, direction remote.Direction) (r *CacheRoot, fastUnwind bool) {
+	c.rootsLock.Lock()
+	defer c.rootsLock.Unlock()
+	r, ok := c.roots[root]
+	if !ok {
+		r = &CacheRoot{ready: make(chan struct{})}
+	}
+	if c.latest == "" {
+		fmt.Printf("advance: empty latest: %x\n", root)
+		c.roots[root] = r
+		r.cache = btree.New(32)
+		c.latest = root
+		return r, false
+	}
+
+	//TODO: need check if c.latest hash is still canonical
+	switch direction {
+	case remote.Direction_FORWARD:
+		fmt.Printf("advance: clone: %x\n", c.latest)
+		r.cache = c.roots[c.latest].cache.Clone()
+	case remote.Direction_UNWIND:
+		fmt.Printf("unwind: %x\n", c.latest)
+		oldRoot, ok := c.roots[root]
+		if ok {
+			r = oldRoot
+			fastUnwind = true
+		} else {
+			r.cache = btree.New(32)
+		}
+	default:
+		panic("not implemented yet")
+	}
+	c.roots[root] = r
+	c.latest = root
+	return r, fastUnwind
+}
+
+func (c *Cache) OnNewBlock(sc *remote.StateChange) {
+	//TODO: clone right root
+	h := gointerfaces.ConvertH256ToHash(sc.BlockHash)
+	root := make([]byte, 40)
+	binary.BigEndian.PutUint64(root, sc.BlockHeight)
+	copy(root[8:], h[:])
+	r, _ := c.advanceRoot(string(root), sc.Direction)
+	r.lock.Lock()
+	for i := range sc.Changes {
+		switch sc.Changes[i].Action {
+		case remote.Action_UPSERT:
+			addr := gointerfaces.ConvertH160toAddress(sc.Changes[i].Address)
+			v := sc.Changes[i].Data
+			r.cache.ReplaceOrInsert(&Pair{K: addr[:], V: v})
+		case remote.Action_DELETE:
+			addr := gointerfaces.ConvertH160toAddress(sc.Changes[i].Address)
+			r.cache.ReplaceOrInsert(&Pair{K: addr[:], V: nil})
+		case remote.Action_CODE, remote.Action_UPSERT_CODE:
+		//skip
+		case remote.Action_STORAGE:
+			addr := gointerfaces.ConvertH160toAddress(sc.Changes[i].Address)
+			for _, change := range sc.Changes[i].StorageChanges {
+				loc := gointerfaces.ConvertH256ToHash(change.Location)
+				k := make([]byte, 20+8+32)
+				copy(k, addr[:])
+				binary.BigEndian.PutUint64(k[20:], sc.Changes[i].Incarnation)
+				copy(k[20+8:], loc[:])
+				r.cache.ReplaceOrInsert(&Pair{K: addr[:], V: change.Data})
+			}
+		default:
+			panic("not implemented yet")
+		}
+	}
+	r.lock.Unlock()
+	close(r.ready) //broadcast
+}
+
+func (c *Cache) View(tx kv.Tx) (*CacheRoot, error) {
+	//TODO: handle case when db has no records
+	encBlockNum, err := tx.GetOne(kv.SyncStageProgress, []byte("Finish"))
+	if err != nil {
+		return nil, err
+	}
+	blockHash, err := tx.GetOne(kv.HeaderCanonical, encBlockNum)
+	if err != nil {
+		return nil, err
+	}
+	root := make([]byte, 8+32)
+	copy(root, encBlockNum)
+	copy(root[8:], blockHash)
+	c.rootsLock.RLock()
+	doBlock := c.latest != ""
+	c.rootsLock.RUnlock()
+
+	fmt.Printf("choose root: %x\n", root)
+	r := c.selectOrCreateRoot(string(root))
+	if doBlock {
+		<-r.ready
+	}
+	return r, nil
+}
+
+func (c *CacheRoot) Get(k []byte, tx kv.Tx) ([]byte, error) {
+	c.lock.RLock()
+	it := c.cache.Get(&Pair{K: k})
+	c.lock.RUnlock()
+
+	if it == nil {
+		v, err := tx.GetOne(kv.PlainState, k)
+		if err != nil {
+			return nil, err
+		}
+
+		c.lock.RLock()
+		it = &Pair{K: k, V: v}
+		c.cache.ReplaceOrInsert(it)
+		c.lock.RUnlock()
+		fmt.Printf("from db: %#x,%#v\n", k, v)
+	} else {
+		fmt.Printf("from cache: %#x,%#v\n", k, it.(*Pair).V)
+	}
+	return it.(*Pair).V, nil
+}
+
+//var g singleflight.Group
+//
+//func A(key, kv kv.RoDB) {
+//	g.Do("", key)
+//	singleflight.Group{}
+//}
