6e310b26697eb1ebf2090d977a67848b055b694e
alex.sharov <AskAlexSharov@gmail.com>
2021-09-11 09:04:43 +0700

    save

--- kv/kvcache/dummy.go
+++ kv/kvcache/dummy.go
@@ -1,6 +1,8 @@
 package kvcache
 
 import (
+	"context"
+
 	"github.com/ledgerwatch/erigon-lib/gointerfaces/remote"
 	"github.com/ledgerwatch/erigon-lib/kv"
 )
@@ -14,8 +16,8 @@ var _ CacheView = (*DummyView)(nil) // compile-time interface check
 
 var dummyView = &DummyView{}
 
-func NewDummy() *DummyCache                                 { return &DummyCache{} }
-func (c *DummyCache) View(tx kv.Tx) (CacheView, error)      { return dummyView, nil }
-func (c *DummyCache) OnNewBlock(sc *remote.StateChange)     {}
-func (c *DummyCache) Evict()                                {}
-func (c *DummyView) Get(k []byte, tx kv.Tx) ([]byte, error) { return tx.GetOne(kv.PlainState, k) }
+func NewDummy() *DummyCache                                                 { return &DummyCache{} }
+func (c *DummyCache) View(ctx context.Context, tx kv.Tx) (CacheView, error) { return dummyView, nil }
+func (c *DummyCache) OnNewBlock(sc *remote.StateChange)                     {}
+func (c *DummyCache) Evict()                                                {}
+func (c *DummyView) Get(k []byte, tx kv.Tx) ([]byte, error)                 { return tx.GetOne(kv.PlainState, k) }
