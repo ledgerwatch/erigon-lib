4f3c9e15ad5976c09b17ebe3c8d37d7c1baef482
Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>
2021-09-12 17:45:45 +0100

    Move licence

--- tools.go
+++ tools.go
@@ -1,3 +1,5 @@
+// +build tools
+
 /*
    Copyright 2021 Erigon contributors
 
@@ -13,8 +15,6 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-
-// +build tools
 
 package tools
 
