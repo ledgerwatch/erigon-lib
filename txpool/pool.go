7c3cf63c6e5e9728768e67cd7c2ff56380f352b7
alex.sharov <AskAlexSharov@gmail.com>
2021-09-14 13:12:27 +0700

    save

--- txpool/pool.go
+++ txpool/pool.go
@@ -144,7 +144,7 @@ const QueuedSubPool SubPoolType = 3
 
 const PendingSubPoolLimit = 10 * 1024
 const BaseFeeSubPoolLimit = 10 * 1024
-const QueuedSubPoolLimit = 30 * 1024
+const QueuedSubPoolLimit = 100 * 1024
 
 const MaxSendersInfoCache = 2 * (PendingSubPoolLimit + BaseFeeSubPoolLimit + QueuedSubPoolLimit)
 
