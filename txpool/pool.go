c706807bd99eeb8d21a8f437d7e11841031281a5
alex.sharov <AskAlexSharov@gmail.com>
2021-09-19 14:53:11 +0700

    isLocal improve

--- txpool/pool.go
+++ txpool/pool.go
@@ -80,9 +80,9 @@ var DefaultConfig = Config{
 	LogEvery:              30 * time.Second,
 	CacheEvictEvery:       1 * time.Minute,
 
-	PendingSubPoolLimit: 40_000,
-	BaseFeeSubPoolLimit: 100_000,
-	QueuedSubPoolLimit:  40_000,
+	PendingSubPoolLimit: 50_000,
+	BaseFeeSubPoolLimit: 50_000,
+	QueuedSubPoolLimit:  50_000,
 }
 
 // Pool is interface for the transaction pool
