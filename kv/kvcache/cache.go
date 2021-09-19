ebf85bde97c3180acb956fbe7f09e49938d7c0f5
alex.sharov <AskAlexSharov@gmail.com>
2021-09-19 11:26:11 +0700

    isLocal improve

--- kv/kvcache/cache.go
+++ kv/kvcache/cache.go
@@ -110,7 +110,7 @@ type CoherentCacheConfig struct {
 var DefaultCoherentCacheConfig = CoherentCacheConfig{
 	KeepViews:    50,
 	NewBlockWait: 50 * time.Millisecond,
-	KeysLimit:    150_000,
+	KeysLimit:    200_000,
 	MetricsLabel: "default",
 	WithStorage:  true,
 }
