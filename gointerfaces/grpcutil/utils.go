286d36bfe19b71abe1025039698d71c1b7c886a1
alex.sharov <AskAlexSharov@gmail.com>
2021-09-26 12:30:58 +0700

    save

--- gointerfaces/grpcutil/utils.go
+++ gointerfaces/grpcutil/utils.go
@@ -63,7 +63,7 @@ func NewServer(rateLimit uint32, creds credentials.TransportCredentials) *grpc.S
 	//cpus := uint32(runtime.GOMAXPROCS(-1))
 	opts := []grpc.ServerOption{
 		//grpc.NumStreamWorkers(cpus), // reduce amount of goroutines
-		grpc.WriteBufferSize(0), // reduce buffers to save mem
+		//grpc.WriteBufferSize(0), // reduce buffers to save mem
 		//grpc.ReadBufferSize(1024),
 		grpc.MaxConcurrentStreams(rateLimit), // to force clients reduce concurrency level
 		// Don't drop the connection, settings accordign to this comment on GitHub
