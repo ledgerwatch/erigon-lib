daad429ea3b9da4e8e26458437bc5b474b62e8ed
alex.sharov <AskAlexSharov@gmail.com>
2021-09-27 17:29:12 +0700

    save

--- txpool/pool.go
+++ txpool/pool.go
@@ -717,16 +717,6 @@ func addTxsOnNewBlock(blockNum uint64, cacheView kvcache.CacheView,
 		onSenderChange(senderID, nonce, balance, byNonce, protocolBaseFee, pendingBaseFee, pending, baseFee, queued, false)
 	}
 
-	for senderID, byNonceSet := range byNonce.bySenderID {
-		if byNonceSet == nil || byNonceSet.Len() == 0 {
-			continue
-		}
-		nonce, balance, err := senders.info(cacheView, senderID)
-		if err != nil {
-			return err
-		}
-		onSenderChange(senderID, nonce, balance, byNonce, protocolBaseFee, pendingBaseFee, pending, baseFee, queued, true)
-	}
 	defer func(t time.Time) { fmt.Printf("pool.go:690: %s\n", time.Since(t)) }(time.Now())
 	pending.EnforceWorstInvariants()
 	baseFee.EnforceInvariants()
