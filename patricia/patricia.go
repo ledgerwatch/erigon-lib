39ae63c0737644551f5d55d7d794c53ec1e299c0
Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>
2021-10-02 22:00:17 +0100

    Now with fuzz tests

--- patricia/patricia.go
+++ patricia/patricia.go
@@ -25,12 +25,12 @@ import (
 // Implementation of paticia tree for efficient search of substrings from a dictionary in a given string
 
 type node struct {
-	p0    uint32 // Number of bits in the left prefix is encoded into the lower 5 bits, the remaining 27 bits are left prefix
-	p1    uint32 // Number of bits in the right prefix is encoding into the lower 5 bits, the remaining 27 bits are right prefix
-	size  uint64
-	n0    *node
-	n1    *node
-	value [][]byte
+	p0 uint32 // Number of bits in the left prefix is encoded into the lower 5 bits, the remaining 27 bits are left prefix
+	p1 uint32 // Number of bits in the right prefix is encoding into the lower 5 bits, the remaining 27 bits are right prefix
+	//size   uint64
+	n0     *node
+	n1     *node
+	values [][]byte
 }
 
 func tostr(x uint32) string {
@@ -55,6 +55,14 @@ func (n *node) print(sb *strings.Builder, indent string) {
 	sb.WriteString("\n")
 	if n.n1 != nil {
 		n.n1.print(sb, indent+"    ")
+	}
+	if len(n.values) > 0 {
+		sb.WriteString(indent)
+		sb.WriteString("vals:")
+		for _, v := range n.values {
+			fmt.Fprintf(sb, " %x", v)
+		}
+		sb.WriteString("\n")
 	}
 }
 
@@ -84,7 +92,7 @@ func (s *state) transition(b byte) uint32 {
 	bitsLeft := 8 // Bits in b to process
 	b32 := uint32(b) << 24
 	for bitsLeft > 0 {
-		//fmt.Printf("bitsLeft = %d, b32 = %x\n", bitsLeft, b32)
+		//fmt.Printf("bitsLeft = %d, b32 = %x, s.head = %s, s.tail = %s\n", bitsLeft, b32, tostr(s.head), tostr(s.tail))
 		if s.head == 0 {
 			// tail has not been determined yet, do it now
 			if b32&0x80000000 == 0 {
@@ -92,19 +100,24 @@ func (s *state) transition(b byte) uint32 {
 			} else {
 				s.tail = s.n.p1
 			}
+			//fmt.Printf("s.tail <- %s\n", tostr(s.tail))
 		}
 		if s.tail == 0 {
 			return b32 | uint32(bitsLeft)
 		}
 		tailLen := int(s.tail & 0x1f)
 		firstDiff := bits.LeadingZeros32(s.tail ^ b32) // First bit where b32 and tail are different
+		//fmt.Printf("firstDiff = %d, bitsLeft = %d\n", firstDiff, bitsLeft)
 		if firstDiff < bitsLeft {
-			bitsLeft -= firstDiff
-			b32 <<= firstDiff
+			//fmt.Printf("firstDiff < bitsLeft\n")
 			if firstDiff >= tailLen {
+				//fmt.Printf("firstDiff >= tailLen\n")
+				bitsLeft -= tailLen
+				b32 <<= tailLen
 				// Need to switch to the next node
-				if s.head&0x80000000 == 0 {
+				if (s.head == 0 && s.tail&0x80000000 == 0) || (s.head != 0 && s.head&0x80000000 == 0) {
 					if s.n.n0 == nil {
+						//fmt.Printf("new node n0\n")
 						s.n.n0 = &node{}
 						if b32&0x80000000 == 0 {
 							s.n.n0.p0 = b32 | uint32(bitsLeft)
@@ -115,6 +128,7 @@ func (s *state) transition(b byte) uint32 {
 					s.n = s.n.n0
 				} else {
 					if s.n.n1 == nil {
+						//fmt.Printf("new node n1\n")
 						s.n.n1 = &node{}
 						if b32&0x80000000 == 0 {
 							s.n.n1.p0 = b32 | uint32(bitsLeft)
@@ -124,9 +138,12 @@ func (s *state) transition(b byte) uint32 {
 					}
 					s.n = s.n.n1
 				}
+				//fmt.Printf("s.n = %p\n", s.n)
 				s.head = 0
 				s.tail = 0
 			} else {
+				bitsLeft -= firstDiff
+				b32 <<= firstDiff
 				// there is divergence, move head and tail
 				mask := ^(uint32(1)<<(32-firstDiff) - 1)
 				s.head |= (s.tail & mask) >> (s.head & 0x1f)
@@ -136,10 +153,11 @@ func (s *state) transition(b byte) uint32 {
 				return b32 | uint32(bitsLeft)
 			}
 		} else if tailLen < bitsLeft {
+			//fmt.Printf("tailLen < bitsLeft\n")
 			bitsLeft -= tailLen
 			b32 <<= tailLen
 			// Switch to the next node
-			if s.head&0x80000000 == 0 {
+			if (s.head == 0 && s.tail&0x80000000 == 0) || (s.head != 0 && s.head&0x80000000 == 0) {
 				if s.n.n0 == nil {
 					s.n.n0 = &node{}
 					if b32&0x80000000 == 0 {
@@ -170,29 +188,25 @@ func (s *state) transition(b byte) uint32 {
 			s.tail = (s.tail&0xffffffe0)<<bitsLeft | (s.tail & 0x1f)
 			s.tail -= uint32(bitsLeft)
 			bitsLeft = 0
+			if s.tail == 0 {
+				if s.head&0x80000000 == 0 {
+					if s.n.n0 != nil {
+						s.n = s.n.n0
+						s.head = 0
+					}
+				} else {
+					if s.n.n1 != nil {
+						s.n = s.n.n1
+						s.head = 0
+					}
+				}
+			}
 		}
 	}
 	return 0
 }
 
 func (s *state) diverge(divergence uint32) {
-	/*
-		if divergence&0x80000000 == 0 {
-			if s.n.p0 == 0 {
-				s.n.p0 = divergence
-				s.head = divergence
-				s.tail = 0
-				return
-			}
-		} else {
-			if s.n.p1 == 0 {
-				s.n.p1 = divergence
-				s.head = divergence
-				s.tail = 0
-				return
-			}
-		}
-	*/
 	if s.tail == 0 {
 		// try to add to the existing head
 		//fmt.Printf("adding divergence to existing head\n")
@@ -207,7 +221,7 @@ func (s *state) diverge(divergence uint32) {
 			s.head += uint32(27 - headLen)
 			//fmt.Printf("s.head %s\n", tostr(s.head))
 			var dn node
-			if s.head&0x80000000 == 0 {
+			if (s.head == 0 && s.tail&0x80000000 == 0) || (s.head != 0 && s.head&0x80000000 == 0) {
 				s.n.p0 = s.head
 				s.n.n0 = &dn
 			} else {
@@ -227,7 +241,7 @@ func (s *state) diverge(divergence uint32) {
 		s.head |= (d32 & mask) >> headLen
 		s.head += uint32(dLen)
 		//fmt.Printf("s.head %s\n", tostr(s.head))
-		if s.head&0x80000000 == 0 {
+		if (s.head == 0 && s.tail&0x80000000 == 0) || (s.head != 0 && s.head&0x80000000 == 0) {
 			s.n.p0 = s.head
 		} else {
 			s.n.p1 = s.head
@@ -239,7 +253,7 @@ func (s *state) diverge(divergence uint32) {
 	if divergence&0x80000000 == 0 {
 		dn.p0 = divergence
 		dn.p1 = s.tail
-		if s.head&0x80000000 == 0 {
+		if (s.head == 0 && s.tail&0x80000000 == 0) || (s.head != 0 && s.head&0x80000000 == 0) {
 			dn.n1 = s.n.n0
 		} else {
 			dn.n1 = s.n.n1
@@ -247,13 +261,13 @@ func (s *state) diverge(divergence uint32) {
 	} else {
 		dn.p1 = divergence
 		dn.p0 = s.tail
-		if s.head&0x80000000 == 0 {
+		if (s.head == 0 && s.tail&0x80000000 == 0) || (s.head != 0 && s.head&0x80000000 == 0) {
 			dn.n0 = s.n.n0
 		} else {
 			dn.n0 = s.n.n1
 		}
 	}
-	if s.head&0x80000000 == 0 {
+	if (s.head == 0 && s.tail&0x80000000 == 0) || (s.head != 0 && s.head&0x80000000 == 0) {
 		s.n.n0 = &dn
 		s.n.p0 = s.head
 	} else {
@@ -265,7 +279,7 @@ func (s *state) diverge(divergence uint32) {
 	s.tail = 0
 }
 
-func (n *node) insert(key []byte) {
+func (n *node) insert(key []byte, value []byte) {
 	s := makestate(n)
 	for _, b := range key {
 		divergence := s.transition(b)
@@ -273,4 +287,39 @@ func (n *node) insert(key []byte) {
 			s.diverge(divergence)
 		}
 	}
+	//fmt.Printf("n.insert before s.insert: %s\n", s)
+	s.insert(value)
+	//fmt.Printf("n.insert after s.insert: %s\n", s)
+}
+
+func (s *state) insert(value []byte) {
+	if s.tail != 0 {
+		s.diverge(0)
+	}
+	if s.head != 0 {
+		var dn node
+		if s.head&0x80000000 == 0 {
+			s.n.n0 = &dn
+		} else {
+			s.n.n1 = &dn
+		}
+		s.n = &dn
+		s.head = 0
+	}
+	s.n.values = append(s.n.values, value)
+}
+
+func (n *node) get(key []byte) ([][]byte, bool) {
+	s := makestate(n)
+	for _, b := range key {
+		divergence := s.transition(b)
+		//fmt.Printf("get %x, b = %x, divergence = %s\nstate=%s\n", key, b, tostr(divergence), s)
+		if divergence != 0 {
+			return nil, false
+		}
+	}
+	if s.tail != 0 {
+		return nil, false
+	}
+	return s.n.values, true
 }
--- patricia/patricia_test.go
+++ patricia/patricia_test.go
@@ -21,36 +21,68 @@ import (
 	"testing"
 )
 
-func TestInserts(t *testing.T) {
+func TestInserts1(t *testing.T) {
 	n := &node{}
 	s := makestate(n)
 	d := s.transition(0x34)
-	fmt.Printf("1 tree:\n%s\nstate: %s\ndivergence %s\n", n, s, tostr(d))
+	fmt.Printf("1 tree:\n%sstate: %s\ndivergence %s\n\n", n, s, tostr(d))
 	s.diverge(d)
-	fmt.Printf("2 tree:\n%s\nstate: %s\n", n, s)
+	fmt.Printf("2 tree:\n%sstate: %s\n\n", n, s)
 	d = s.transition(0x56)
-	fmt.Printf("3 tree:\n%s\nstate: %s\ndivergence %s\n", n, s, tostr(d))
+	fmt.Printf("3 tree:\n%sstate: %s\ndivergence %s\n\n", n, s, tostr(d))
 	s.diverge(d)
-	fmt.Printf("4 tree:\n%s\nstate: %s\n", n, s)
+	fmt.Printf("4 tree:\n%sstate: %s\n\n", n, s)
 	d = s.transition(0xff)
-	fmt.Printf("5 tree:\n%s\nstate: %s\ndivergence %s\n", n, s, tostr(d))
+	fmt.Printf("5 tree:\n%sstate: %s\ndivergence %s\n\n", n, s, tostr(d))
 	s.diverge(d)
-	fmt.Printf("6 tree:\n%s\nstate: %s\n", n, s)
+	fmt.Printf("6 tree:\n%sstate: %s\n\n", n, s)
 	d = s.transition(0xcc)
-	fmt.Printf("7 tree:\n%s\nstate: %s\ndivergence %s\n", n, s, tostr(d))
+	fmt.Printf("7 tree:\n%sstate: %s\ndivergence %s\n\n", n, s, tostr(d))
 	s.diverge(d)
-	fmt.Printf("8 tree:\n%s\nstate: %s\n", n, s)
+	fmt.Printf("8 tree:\n%sstate: %s\n\n", n, s)
+	s.insert(nil)
 	s = makestate(n)
 	d = s.transition(0x34)
-	fmt.Printf("9 tree:\n%s\nstate: %s\ndivergence %s\n", n, s, tostr(d))
+	fmt.Printf("9 tree:\n%sstate: %s\ndivergence %s\n\n", n, s, tostr(d))
 	d = s.transition(0x66)
-	fmt.Printf("10 tree:\n%s\nstate: %s\ndivergence %s\n", n, s, tostr(d))
+	fmt.Printf("10 tree:\n%sstate: %s\ndivergence %s\n\n", n, s, tostr(d))
 	s.diverge(d)
-	fmt.Printf("11 tree:\n%s\nstate: %s\n", n, s)
+	fmt.Printf("11 tree:\n%sstate: %s\n\n", n, s)
 
-	n.insert([]byte{0xff, 0xff, 0xff, 0xff, 0xff})
+	n.insert([]byte{0xff, 0xff, 0xff, 0xff, 0xff}, []byte{0x01})
 	fmt.Printf("12 tree:\n%s\n", n)
 
-	n.insert([]byte{0xff, 0xff, 0xff, 0xff, 0x0f})
-	fmt.Printf("12 tree:\n%s\n", n)
+	n.insert([]byte{0xff, 0xff, 0xff, 0xff, 0x0f}, []byte{0x02})
+	fmt.Printf("13 tree:\n%s\n", n)
+
+	n.insert([]byte{0xff, 0xff, 0xff, 0xff, 0xff}, []byte{0x03})
+	fmt.Printf("14 tree:\n%s\n", n)
+
+	vs, ok := n.get([]byte{0xff, 0xff, 0xff, 0xff, 0x0f})
+	fmt.Printf("15 vs = %x, ok = %t\n", vs, ok)
+
+	vs, ok = n.get([]byte{0xff, 0xff, 0xff, 0xff, 0xff})
+	fmt.Printf("16 vs = %x, ok = %t\n", vs, ok)
+
+	vs, ok = n.get([]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0x56})
+	fmt.Printf("17 vs = %x, ok = %t\n", vs, ok)
+
+	vs, ok = n.get([]byte{0x34, 0x56, 0xff, 0xcc})
+	fmt.Printf("18 vs = %x, ok = %t\n", vs, ok)
+
+	vs, ok = n.get([]byte{})
+	fmt.Printf("19 vs = %x, ok = %t\n", vs, ok)
+}
+
+func TestInserts2(t *testing.T) {
+	var n node
+	n.insert([]byte{0xff}, []byte{0x03, 0x03, 0x03, 0x1a, 0xed, 0xed})
+	n.insert([]byte{0xed}, []byte{})
+	fmt.Printf("tree:\n%s", &n)
+
+	vs, ok := n.get([]byte{0xff})
+	fmt.Printf("vs = %x, ok = %t\n", vs, ok)
+
+	vs, ok = n.get([]byte{0xed})
+	fmt.Printf("vs = %x, ok = %t\n", vs, ok)
 }
