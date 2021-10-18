df32832fbcbb23f6e0780a265d020205f5c73a60
alex.sharov <AskAlexSharov@gmail.com>
2021-10-18 11:16:31 +0700

    asdf

--- txpool/pool.go
+++ txpool/pool.go
@@ -22,7 +22,9 @@ import (
 	"context"
 	"encoding/binary"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"math"
 	"runtime"
 	"sort"
@@ -44,6 +46,7 @@ import (
 	"github.com/ledgerwatch/erigon-lib/kv/mdbx"
 	"github.com/ledgerwatch/log/v3"
 	"go.uber.org/atomic"
+	"google.golang.org/grpc/status"
 )
 
 var (
@@ -1141,17 +1144,15 @@ func MainLoop(ctx context.Context, db kv.RwDB, coreDB kv.RoDB, p *TxPool, newTxs
 			if !p.Started() {
 				continue
 			}
-			/*
-				if err := p.processRemoteTxs(ctx); err != nil {
-					if s, ok := status.FromError(err); ok && retryLater(s.Code()) {
-						continue
-					}
-					if errors.Is(err, io.EOF) || errors.Is(err, context.Canceled) {
-						continue
-					}
-					log.Error("[txpool] process batch remote txs", "err", err)
+			if err := p.processRemoteTxs(ctx); err != nil {
+				if s, ok := status.FromError(err); ok && retryLater(s.Code()) {
+					continue
 				}
-			*/
+				if errors.Is(err, io.EOF) || errors.Is(err, context.Canceled) {
+					continue
+				}
+				log.Error("[txpool] process batch remote txs", "err", err)
+			}
 		case <-commitEvery.C:
 			if db != nil {
 				t := time.Now()
