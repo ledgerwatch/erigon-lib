beb1a3f4c459a1bdfa2fc1bad48c9ddefc52158b
alex.sharov <AskAlexSharov@gmail.com>
2021-10-21 16:56:21 +0700

    save

--- common/debug/log_panic.go
+++ common/debug/log_panic.go
@@ -6,13 +6,14 @@ import (
 	"strings"
 )
 
-var panicReplacer = strings.NewReplacer("\n", " ", "\t", "", "\r", "")
+// PanicReplacer - does format panic to make it readable in our logs
+var PanicReplacer = strings.NewReplacer("\n", " ", "\t", "", "\r", "")
 
 // Recover - does save panic to datadir/crashreports, bud doesn't log to logger and doesn't stop the process
 // it returns recovered panic as error in format friendly for our logger
 // common pattern of use - assign to named output param:
 //  func A() (err error) {
-//	    defer func() { err = debug.ReportPanicAndRecover() }() // avoid crash because Erigon's core does many things
+//	    defer func() { err = debug.Recover(err) }() // avoid crash because Erigon's core does many things
 //  }
 func Recover(err error) error {
 	panicResult := recover()
@@ -23,9 +24,9 @@ func Recover(err error) error {
 	stack := string(debug.Stack())
 	switch typed := panicResult.(type) {
 	case error:
-		err = fmt.Errorf("%w, trace: %s", typed, panicReplacer.Replace(stack))
+		err = fmt.Errorf("%w, trace: %s", typed, PanicReplacer.Replace(stack))
 	default:
-		err = fmt.Errorf("%+v, trace: %s", typed, panicReplacer.Replace(stack))
+		err = fmt.Errorf("%+v, trace: %s", typed, PanicReplacer.Replace(stack))
 	}
 	return err
 }
