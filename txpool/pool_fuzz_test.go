8afda1e68063bb0fb0d9e2c7de4a914c2c67c152
alex.sharov <AskAlexSharov@gmail.com>
2021-10-22 14:57:51 +0700

    pool intrinsic gas

--- txpool/pool_fuzz_test.go
+++ txpool/pool_fuzz_test.go
@@ -308,7 +308,7 @@ func FuzzOnNewBlocks(f *testing.F) {
 	f.Add(u64[:], u64[:], u64[:], u64[:], senderAddr[:], uint8(14))
 	f.Add(u64[:], u64[:], u64[:], u64[:], senderAddr[:], uint8(123))
 	f.Fuzz(func(t *testing.T, txNonce, values, tips, feeCap, senderAddr []byte, pendingBaseFee1 uint8) {
-		//t.Parallel()
+		t.Parallel()
 		ctx := context.Background()
 
 		pendingBaseFee := uint64(pendingBaseFee1%16 + 1)
