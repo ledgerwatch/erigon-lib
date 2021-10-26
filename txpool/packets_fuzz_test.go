de4c5fc11a9bd48b609349654c93a2f481c6a7f0
alex.sharov <AskAlexSharov@gmail.com>
2021-10-26 12:01:09 +0700

    save

--- txpool/packets_fuzz_test.go
+++ txpool/packets_fuzz_test.go
@@ -4,6 +4,7 @@
 package txpool
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/ledgerwatch/erigon-lib/common/u256"
@@ -14,6 +15,7 @@ func FuzzPooledTransactions66(f *testing.F) {
 	f.Add(decodeHex("f8d7820457f8d2f867088504a817c8088302e2489435353535353535353535353535353535353535358202008025a064b1702d9298fee62dfeccc57d322a463ad55ca201256d01f62b45b2e1c21c12a064b1702d9298fee62dfeccc57d322a463ad55ca201256d01f62b45b2e1c21c10f867098504a817c809830334509435353535353535353535353535353535353535358202d98025a052f8f61201b2b11a78d6e866abc9c3db2ae8631fa656bfe5cb53668255367afba052f8f61201b2b11a78d6e866abc9c3db2ae8631fa656bfe5cb53668255367afb"))
 	f.Fuzz(func(t *testing.T, in []byte) {
 		t.Parallel()
+		fmt.Printf("%x\n", in)
 		ctx := NewTxParseContext(*u256.N1)
 		slots := TxSlots{}
 		reqId, _, err := ParsePooledTransactions66(in, 0, ctx, &slots)
