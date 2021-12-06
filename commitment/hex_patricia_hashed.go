da6eabc3ca5b8dc8422d6344946fa3d036af14f4
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2021-12-06 14:00:47 +0000

    More

--- commitment/hex_patricia_hashed.go
+++ commitment/hex_patricia_hashed.go
@@ -26,9 +26,9 @@ type HexPatriciaHashed struct {
 	root Cell // Root cell of the tree
 	// Rows of the grid correspond to the level of depth in the patricia tree
 	// Columns of the grid correspond to pointers to the nodes further from the root
-	grid    [128][16]Cell // First 64 rows of this grid are for account trie, and next 64 rows are for storage trie
-	account AccountCell   // Currently accessed account
-	storage [32]byte      // Currently access storage value
+	grid     [128][16]Cell   // First 64 rows of this grid are for account trie, and next 64 rows are for storage trie
+	accounts [16]AccountCell // Account cells that augument non-account cells in given column
+	storages [16][32]byte    // Storage cells that augument non-storage cells in given column
 	// How many rows (starting from row 0) are currently active and have corresponding selected columns
 	// Last active row does not have selected column
 	activeRows     int
@@ -39,7 +39,7 @@ type HexPatriciaHashed struct {
 	// For each cell, it sets the cell type, clears the modified flag, fills the hash,
 	// and for the extension, account, and leaf type, the `l` and `k`
 	branchFn func(prefix []byte, row []Cell) error
-	// Function used to fetch account with given plain key
+	// Function used to fetch account with given plain key. It loads
 	accountFn func(plainKey []byte, accountCell *AccountCell) error
 	// Function used to fetch account with given plain key
 	storageFn func(plainKey []byte, storageCell []byte) error
@@ -59,22 +59,30 @@ const (
 	EMBEDDED_CELL  // Embedded leaf (RLP representation is less than 32 bytes)
 )
 
+type CellMode byte
+
+const (
+	NONE_MODE CellMode = iota
+	MODIFIED_MODE
+	DELETED_MODE
+	NEW_MODE
+)
+
 type Cell struct {
 	h  [32]byte  // cell hash
-	hk [128]byte // part of the hashd key (for the extension and leaf nodes), one byte contains one nibble (hex digit 0-15)
+	hk [128]byte // part of the hashed key (for the extension and leaf nodes), one byte contains one nibble (hex digit 0-15)
 	hl byte      // length (for the hashed key part)
 	pk [52]byte  // plain key
 	pl byte      // length (for the plain key)
 	t  CellType  // cell type
-	m  bool      // modified
-	d  bool      // deleted
+	m  CellMode  // mode - whether cell is modified or deleted or new
 }
 
 type AccountCell struct {
-	Nonce    uint64
-	Balance  uint256.Int
-	Root     [32]byte // merkle root of the storage trie
-	CodeHash [32]byte // hash of the bytecode
+	Nonce      uint64
+	Balance    uint256.Int
+	hasStorage bool     // Whether this account has storage items
+	CodeHash   [32]byte // hash of the bytecode
 }
 
 func (hph *HexPatriciaHashed) unfoldCell(c *Cell, row int) error {
@@ -84,21 +92,20 @@ func (hph *HexPatriciaHashed) unfoldCell(c *Cell, row int) error {
 		for i := 0; i < 16; i++ {
 			c1 := &hph.grid[row][i]
 			c1.t = EMPTY_CELL
-			c1.m = false
-			c1.d = false
+			c1.m = NONE_MODE
 		}
 	case BRANCH_CELL:
 		if err = hph.branchFn(hph.selectedPrefix, hph.grid[row][:]); err != nil {
 			return err
 		}
 	case ACCOUNT_CELL, ACC_INTER_CELL:
+		nibble := c.hk[0]
 		if c.hl == 1 {
 			// If there is only one nibble left in the key, unfold into account
-			if err = hph.accountFn(c.pk[:], &hph.account); err != nil {
+			if err = hph.accountFn(c.pk[:], &hph.accounts[nibble]); err != nil {
 				return err
 			}
 		} else {
-			nibble := c.hk[0]
 			for i := byte(0); i < 16; i++ {
 				c1 := &hph.grid[row][i]
 				if i == nibble {
@@ -110,8 +117,7 @@ func (hph *HexPatriciaHashed) unfoldCell(c *Cell, row int) error {
 				} else {
 					c1.t = EMPTY_CELL
 				}
-				c1.m = false
-				c1.d = false
+				c1.m = NONE_MODE
 			}
 		}
 	case EXTENSION_CELL, EXT_INTER_CELL:
@@ -130,17 +136,16 @@ func (hph *HexPatriciaHashed) unfoldCell(c *Cell, row int) error {
 			} else {
 				c1.t = EMPTY_CELL
 			}
-			c1.m = false
-			c1.d = false
+			c1.m = NONE_MODE
 		}
 	case STORAGE_CELL, STR_INTER_CELL, EMBEDDED_CELL:
+		nibble := c.hk[0]
 		if c.hl == 1 {
 			// If there is only one nibble left in the key, unfold into storage
-			if err = hph.storageFn(c.pk[:], hph.storage[:]); err != nil {
+			if err = hph.storageFn(c.pk[:], hph.storages[nibble][:]); err != nil {
 				return err
 			}
 		} else {
-			nibble := c.hk[0]
 			for i := byte(0); i < 16; i++ {
 				c1 := &hph.grid[row][i]
 				if i == nibble {
@@ -152,8 +157,7 @@ func (hph *HexPatriciaHashed) unfoldCell(c *Cell, row int) error {
 				} else {
 					c1.t = EMPTY_CELL
 				}
-				c1.m = false
-				c1.d = false
+				c1.m = NONE_MODE
 			}
 		}
 	}
