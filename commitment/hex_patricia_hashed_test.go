b2f7aab96f7b5c16dd0a0e13ce3f6d7e963f4579
Alex Sharp <alexsharp@Alexs-MacBook-Pro.local>
2021-12-08 08:53:39 +0000

    More

--- commitment/hex_patricia_hashed_test.go
+++ commitment/hex_patricia_hashed_test.go
@@ -16,7 +16,12 @@
 
 package commitment
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/holiman/uint256"
+	"github.com/ledgerwatch/erigon-lib/common"
+)
 
 // In memory commitment and state to use with the tests
 type MockState struct {
@@ -32,6 +37,59 @@ func (ms MockState) accountFn(plainKey []byte, account *AccountDecorator) error 
 
 func (ms MockState) storageFn(plainKey []byte, storage []byte) error {
 	return nil
+}
+
+// UpdateBuilder collects updates to the state
+// and provides them in properly sorted form
+type UpdateBuilder struct {
+	balances   map[string]*uint256.Int
+	nonces     map[string]uint64
+	codeHashes map[string][32]byte
+	storages   map[string][]byte
+	deletes    map[string]struct{}
+}
+
+func NewUpdateBuilder() *UpdateBuilder {
+	return &UpdateBuilder{
+		balances:   make(map[string]*uint256.Int),
+		nonces:     make(map[string]uint64),
+		codeHashes: make(map[string][32]byte),
+		storages:   make(map[string][]byte),
+		deletes:    make(map[string]struct{}),
+	}
+}
+
+func (ub *UpdateBuilder) Balance(addr []byte, balance *uint256.Int) {
+	sk := string(common.Copy(addr))
+	delete(ub.deletes, sk)
+	ub.balances[sk] = balance.Clone()
+}
+
+func (ub *UpdateBuilder) Nonce(addr []byte, nonce uint64) {
+	sk := string(common.Copy(addr))
+	delete(ub.deletes, sk)
+	ub.nonces[sk] = nonce
+}
+
+func (ub *UpdateBuilder) CodeHash(addr []byte, hash [32]byte) {
+	sk := string(common.Copy(addr))
+	delete(ub.deletes, sk)
+	ub.codeHashes[sk] = hash
+}
+
+func (ub *UpdateBuilder) Storage(key []byte, value []byte) {
+	sk := string(common.Copy(key))
+	delete(ub.deletes, sk)
+	ub.storages[sk] = common.Copy(value)
+}
+
+func (ub *UpdateBuilder) Delete(key []byte) {
+	sk := string(common.Copy(key))
+	delete(ub.balances, sk)
+	delete(ub.nonces, sk)
+	delete(ub.codeHashes, sk)
+	delete(ub.storages, sk)
+	ub.deletes[sk] = struct{}{}
 }
 
 func TestEmptyState(t *testing.T) {
