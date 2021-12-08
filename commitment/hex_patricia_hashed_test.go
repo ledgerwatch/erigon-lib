f3cb79b27c6e2b59db1ab3c8a559c2e1c0674f86
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2021-12-08 17:26:08 +0000

    More to the test

--- commitment/hex_patricia_hashed_test.go
+++ commitment/hex_patricia_hashed_test.go
@@ -17,6 +17,7 @@
 package commitment
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/holiman/uint256"
@@ -47,6 +48,7 @@ type UpdateBuilder struct {
 	codeHashes map[string][32]byte
 	storages   map[string][]byte
 	deletes    map[string]struct{}
+	keyset     map[string]struct{}
 }
 
 func NewUpdateBuilder() *UpdateBuilder {
@@ -56,6 +58,7 @@ func NewUpdateBuilder() *UpdateBuilder {
 		codeHashes: make(map[string][32]byte),
 		storages:   make(map[string][]byte),
 		deletes:    make(map[string]struct{}),
+		keyset:     make(map[string]struct{}),
 	}
 }
 
@@ -63,24 +66,28 @@ func (ub *UpdateBuilder) Balance(addr []byte, balance *uint256.Int) {
 	sk := string(common.Copy(addr))
 	delete(ub.deletes, sk)
 	ub.balances[sk] = balance.Clone()
+	ub.keyset[sk] = struct{}{}
 }
 
 func (ub *UpdateBuilder) Nonce(addr []byte, nonce uint64) {
 	sk := string(common.Copy(addr))
 	delete(ub.deletes, sk)
 	ub.nonces[sk] = nonce
+	ub.keyset[sk] = struct{}{}
 }
 
 func (ub *UpdateBuilder) CodeHash(addr []byte, hash [32]byte) {
 	sk := string(common.Copy(addr))
 	delete(ub.deletes, sk)
 	ub.codeHashes[sk] = hash
+	ub.keyset[sk] = struct{}{}
 }
 
 func (ub *UpdateBuilder) Storage(key []byte, value []byte) {
 	sk := string(common.Copy(key))
 	delete(ub.deletes, sk)
 	ub.storages[sk] = common.Copy(value)
+	ub.keyset[sk] = struct{}{}
 }
 
 func (ub *UpdateBuilder) Delete(key []byte) {
@@ -90,6 +97,56 @@ func (ub *UpdateBuilder) Delete(key []byte) {
 	delete(ub.codeHashes, sk)
 	delete(ub.storages, sk)
 	ub.deletes[sk] = struct{}{}
+	ub.keyset[sk] = struct{}{}
+}
+
+type UpdateFlags uint8
+
+const (
+	BALANCE_UPDATE UpdateFlags = 1
+	NONCE_UPDATE   UpdateFlags = 2
+	CODE_UPDATE    UpdateFlags = 4
+	STORAGE_UPDATE UpdateFlags = 8
+)
+
+type Update struct {
+	flags             UpdateFlags
+	balance           uint256.Int
+	nonce             uint64
+	codeHashOrStorage [32]byte
+}
+
+// Build
+func (ub *UpdateBuilder) Build() ([][]byte, []Update) {
+	var keys []string
+	for key := range ub.keyset {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	returnKeys := make([][]byte, len(keys))
+	returnUpdates := make([]Update, len(keys))
+	for i, key := range keys {
+		returnKeys[i] = []byte(key)
+		u := &returnUpdates[i]
+		if balance, ok := ub.balances[key]; ok {
+			u.flags |= BALANCE_UPDATE
+			u.balance.Set(balance)
+		}
+		if nonce, ok := ub.nonces[key]; ok {
+			u.flags |= NONCE_UPDATE
+			u.nonce = nonce
+		}
+		if codeHash, ok := ub.codeHashes[key]; ok {
+			u.flags |= CODE_UPDATE
+			copy(u.codeHashOrStorage[:], codeHash[:])
+		}
+		if storage, ok := ub.storages[key]; ok {
+			u.flags |= STORAGE_UPDATE
+			u.codeHashOrStorage = [32]byte{}
+			copy(u.codeHashOrStorage[32-len(storage):], storage)
+		}
+	}
+	return returnKeys, returnUpdates
 }
 
 func TestEmptyState(t *testing.T) {
