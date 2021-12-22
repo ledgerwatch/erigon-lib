1c54b2b550470420dabc336191f77b72d579ca56
alex.sharov <AskAlexSharov@gmail.com>
2021-12-22 11:47:40 +0700

    save

--- rlp/parse_test.go
+++ rlp/parse_test.go
@@ -2,7 +2,6 @@ package rlp
 
 import (
 	"encoding/hex"
-	"errors"
 	"fmt"
 	"testing"
 
@@ -22,15 +21,15 @@ var parseU64Tests = []struct {
 	payload   []byte
 	expectPos int
 	expectRes uint64
-	expectErr string
+	expectErr error
 }{
 	{payload: decodeHex("820400"), expectPos: 3, expectRes: 1024},
 	{payload: decodeHex("07"), expectPos: 1, expectRes: 7},
-	{payload: decodeHex("8107"), expectErr: "rlp parse: non-canonical size information"},
-	{payload: decodeHex("B8020004"), expectErr: "rlp parse: non-canonical size information"},
-	{payload: decodeHex("C0"), expectErr: "rlp parse: uint64 must be a string, not isList"},
-	{payload: decodeHex("00"), expectErr: "rlp parse: integer encoding for RLP must not have leading zeros: 00"},
-	{payload: decodeHex("8AFFFFFFFFFFFFFFFFFF7C"), expectErr: "rlp parse: uint64 must not be more than 8 bytes long, got 10"},
+	{payload: decodeHex("8107"), expectErr: fmt.Errorf("%w: non-canonical size information", ErrParse)},
+	{payload: decodeHex("B8020004"), expectErr: fmt.Errorf("%w: non-canonical size information", ErrParse)},
+	{payload: decodeHex("C0"), expectErr: fmt.Errorf("%w: uint64 must be a string, not isList", ErrParse)},
+	{payload: decodeHex("00"), expectErr: fmt.Errorf("%w: integer encoding for RLP must not have leading zeros: 00", ErrParse)},
+	{payload: decodeHex("8AFFFFFFFFFFFFFFFFFF7C"), expectErr: fmt.Errorf("%w: uint64 must not be more than 8 bytes long, got 10", ErrParse)},
 }
 
 var parseU32Tests = []struct {
@@ -41,11 +40,11 @@ var parseU32Tests = []struct {
 }{
 	{payload: decodeHex("820400"), expectPos: 3, expectRes: 1024},
 	{payload: decodeHex("07"), expectPos: 1, expectRes: 7},
-	{payload: decodeHex("8107"), expectErr: errors.New("rlp: non-canonical size information")},
-	{payload: decodeHex("B8020004"), expectErr: errors.New("rlp: non-canonical size information")},
-	{payload: decodeHex("C0"), expectErr: errors.New("uint32 must be a string, not isList")},
-	{payload: decodeHex("00"), expectErr: errors.New("integer encoding for RLP must not have leading zeros: 00")},
-	{payload: decodeHex("85FF6738FF7C"), expectErr: errors.New("uint32 must not be more than 4 bytes long, got 5")},
+	{payload: decodeHex("8107"), expectErr: fmt.Errorf("%w: non-canonical size information", ErrParse)},
+	{payload: decodeHex("B8020004"), expectErr: fmt.Errorf("%w: non-canonical size information", ErrParse)},
+	{payload: decodeHex("C0"), expectErr: fmt.Errorf("%w: uint32 must be a string, not isList", ErrParse)},
+	{payload: decodeHex("00"), expectErr: fmt.Errorf("%w: integer encoding for RLP must not have leading zeros: 00", ErrParse)},
+	{payload: decodeHex("85FF6738FF7C"), expectErr: fmt.Errorf("%w: uint32 must not be more than 4 bytes long, got 5", ErrParse)},
 }
 
 var parseU256Tests = []struct {
@@ -54,16 +53,16 @@ var parseU256Tests = []struct {
 	expectRes *uint256.Int
 	expectErr error
 }{
-	{payload: decodeHex("8BFFFFFFFFFFFFFFFFFF7C"), expectErr: errors.New("unexpected end of payload")},
+	{payload: decodeHex("8BFFFFFFFFFFFFFFFFFF7C"), expectErr: fmt.Errorf("%w: unexpected end of payload", ErrParse)},
 	{payload: decodeHex("8AFFFFFFFFFFFFFFFFFF7C"), expectPos: 11, expectRes: new(uint256.Int).SetBytes(decodeHex("FFFFFFFFFFFFFFFFFF7C"))},
 	{payload: decodeHex("85CE05050505"), expectPos: 6, expectRes: new(uint256.Int).SetUint64(0xCE05050505)},
 	{payload: decodeHex("820400"), expectPos: 3, expectRes: new(uint256.Int).SetUint64(1024)},
 	{payload: decodeHex("07"), expectPos: 1, expectRes: new(uint256.Int).SetUint64(7)},
-	{payload: decodeHex("8107"), expectErr: errors.New("rlp: non-canonical size information")},
-	{payload: decodeHex("B8020004"), expectErr: errors.New("rlp: non-canonical size information")},
-	{payload: decodeHex("C0"), expectErr: errors.New("must be a string, instead of a list")},
-	{payload: decodeHex("00"), expectErr: errors.New("integer encoding for RLP must not have leading zeros: 00")},
-	{payload: decodeHex("A101000000000000000000000000000000000000008B000000000000000000000000"), expectErr: errors.New("uint256 must not be more than 32 bytes long, got 33")},
+	{payload: decodeHex("8107"), expectErr: fmt.Errorf("%w: non-canonical size information", ErrParse)},
+	{payload: decodeHex("B8020004"), expectErr: fmt.Errorf("%w: non-canonical size information", ErrParse)},
+	{payload: decodeHex("C0"), expectErr: fmt.Errorf("%w: must be a string, instead of a list", ErrParse)},
+	{payload: decodeHex("00"), expectErr: fmt.Errorf("%w: integer encoding for RLP must not have leading zeros: 00", ErrParse)},
+	{payload: decodeHex("A101000000000000000000000000000000000000008B000000000000000000000000"), expectErr: fmt.Errorf("%w: uint256 must not be more than 32 bytes long, got 33", ErrParse)},
 }
 
 func TestPrimitives(t *testing.T) {
@@ -71,7 +70,7 @@ func TestPrimitives(t *testing.T) {
 		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
 			assert := assert.New(t)
 			pos, res, err := U64(tt.payload, 0)
-			assert.EqualError(err, tt.expectErr)
+			assert.Equal(tt.expectErr, err)
 			assert.Equal(tt.expectPos, pos)
 			assert.Equal(tt.expectRes, res)
 		})
