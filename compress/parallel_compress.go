f547553c8f796b07907e1717afc4e55fb3bf0dc0
alex.sharov <AskAlexSharov@gmail.com>
2022-01-16 11:21:12 +0700

    save

--- compress/parallel_compress.go
+++ compress/parallel_compress.go
@@ -849,7 +849,7 @@ func processSuperstring(superstringCh chan []byte, dictCollector *etl.Collector,
 					}
 				}
 
-				if (l <= 8 && repeats < 1000) ||
+				if (l <= 8 && repeats < 2000) ||
 					(l > 8 && l < 32 && repeats < 200) ||
 					(l >= 32 && repeats < 50) ||
 					(l > 64 && repeats < 500) {
