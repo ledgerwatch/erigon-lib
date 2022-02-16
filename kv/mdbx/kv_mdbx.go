3fd2af0ee45385b5d27d1e72418ef6c3a0d2602b
alex.sharov <AskAlexSharov@gmail.com>
2022-02-16 09:00:57 +0700

    a

--- kv/mdbx/kv_mdbx.go
+++ kv/mdbx/kv_mdbx.go
@@ -206,7 +206,7 @@ func (opts MdbxOpts) Open() (kv.RwDB, error) {
 		}
 		// must be in the range from 12.5% (almost empty) to 50% (half empty)
 		// which corresponds to the range from 8192 and to 32768 in units respectively
-		if err = env.SetOption(mdbx.OptMergeThreshold16dot16Percent, 32768); err != nil {
+		if err = env.SetOption(mdbx.OptMergeThreshold16dot16Percent, 8192); err != nil {
 			return nil, err
 		}
 	}
