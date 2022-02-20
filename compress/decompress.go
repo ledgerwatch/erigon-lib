9b46ffb458179de70c9a89f3534bb3426a904ba5
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2022-02-20 20:54:28 +0000

    Build up huffman tree

--- compress/decompress.go
+++ compress/decompress.go
@@ -26,6 +26,18 @@ import (
 	"github.com/ledgerwatch/erigon-lib/mmap"
 )
 
+type huffmanNodePos struct {
+	zero *huffmanNodePos
+	one  *huffmanNodePos
+	pos  uint64
+}
+
+type huffmanNodePattern struct {
+	zero    *huffmanNodePattern
+	one     *huffmanNodePattern
+	pattern []byte
+}
+
 // Decompressor provides access to the superstrings in a file produced by a compressor
 type Decompressor struct {
 	compressedFile string
@@ -33,8 +45,8 @@ type Decompressor struct {
 	mmapHandle1    []byte                 // mmap handle for unix (this is used to close mmap)
 	mmapHandle2    *[mmap.MaxMapSize]byte // mmap handle for windows (this is used to close mmap)
 	data           []byte                 // slice of correct size for the decompressor to work with
-	dict           Dictionary
-	posDict        Dictionary
+	dict           *huffmanNodePattern
+	posDict        *huffmanNodePos
 	wordsStart     uint64 // Offset of whether the superstrings actually start
 	count          uint64
 	size           int64
@@ -63,16 +75,42 @@ func NewDecompressor(compressedFile string) (*Decompressor, error) {
 	d.data = d.mmapHandle1[:d.size]
 	d.count = binary.BigEndian.Uint64(d.data[:8])
 	dictSize := binary.BigEndian.Uint64(d.data[8:16])
-	d.dict.rootOffset = binary.BigEndian.Uint64(d.data[16:24])
-	d.dict.cutoff = binary.BigEndian.Uint64(d.data[24:32])
-	d.dict.data = d.data[32 : 32+dictSize]
+	rootOffset := binary.BigEndian.Uint64(d.data[16:24])
+	cutoff := binary.BigEndian.Uint64(d.data[24:32])
+	data := d.data[32 : 32+dictSize]
+	if dictSize > 0 {
+		d.dict = buildHuffmanPattern(data, rootOffset, cutoff)
+	}
 	pos := 32 + dictSize
 	dictSize = binary.BigEndian.Uint64(d.data[pos : pos+8])
-	d.posDict.rootOffset = binary.BigEndian.Uint64(d.data[pos+8 : pos+16])
-	d.posDict.cutoff = binary.BigEndian.Uint64(d.data[pos+16 : pos+24])
-	d.posDict.data = d.data[pos+24 : pos+24+dictSize]
+	rootOffset = binary.BigEndian.Uint64(d.data[pos+8 : pos+16])
+	cutoff = binary.BigEndian.Uint64(d.data[pos+16 : pos+24])
+	data = d.data[pos+24 : pos+24+dictSize]
+	if dictSize > 0 {
+		d.posDict = buildHuffmanPos(data, rootOffset, cutoff)
+	}
 	d.wordsStart = pos + 24 + dictSize
 	return d, nil
+}
+
+func buildHuffmanPos(data []byte, offset uint64, cutoff uint64) *huffmanNodePos {
+	if offset < cutoff {
+		pos, _ := binary.Uvarint(data[offset:])
+		return &huffmanNodePos{pos: pos}
+	}
+	offsetZero, n := binary.Uvarint(data[offset:])
+	offsetOne, _ := binary.Uvarint(data[offset+uint64(n):])
+	return &huffmanNodePos{zero: buildHuffmanPos(data, offsetZero, cutoff), one: buildHuffmanPos(data, offsetOne, cutoff)}
+}
+
+func buildHuffmanPattern(data []byte, offset uint64, cutoff uint64) *huffmanNodePattern {
+	if offset < cutoff {
+		l, n := binary.Uvarint(data[offset:])
+		return &huffmanNodePattern{pattern: data[offset+uint64(n) : offset+uint64(n)+l]}
+	}
+	offsetZero, n := binary.Uvarint(data[offset:])
+	offsetOne, _ := binary.Uvarint(data[offset+uint64(n):])
+	return &huffmanNodePattern{zero: buildHuffmanPattern(data, offsetZero, cutoff), one: buildHuffmanPattern(data, offsetOne, cutoff)}
 }
 
 func (d *Decompressor) Size() int64 {
@@ -98,19 +136,13 @@ func (d *Decompressor) WithReadAhead(f func() error) error {
 	return f()
 }
 
-type Dictionary struct {
-	data       []byte
-	rootOffset uint64
-	cutoff     uint64
-}
-
 // Getter represent "reader" or "interator" that can move accross the data of the decompressor
 // The full state of the getter can be captured by saving dataP, b, and mask values.
 type Getter struct {
 	data        []byte
 	dataP       uint64
-	patternDict *Dictionary
-	posDict     *Dictionary
+	patternDict *huffmanNodePattern
+	posDict     *huffmanNodePos
 	b           byte
 	mask        byte
 	uncovered   []int // Buffer for uncovered portions of the word
@@ -118,95 +150,61 @@ type Getter struct {
 	fName       string
 }
 
-func (g *Getter) zero(offset uint64) uint64 {
-	offset, _ = binary.Uvarint(g.patternDict.data[offset:])
-	return offset
-}
-
-func (g *Getter) one(offset uint64) uint64 {
-	_, n := binary.Uvarint(g.patternDict.data[offset:])
-	offset, _ = binary.Uvarint(g.patternDict.data[offset+uint64(n):])
-	return offset
-}
-
-func (g *Getter) posZero(offset uint64) uint64 {
-	offset, _ = binary.Uvarint(g.posDict.data[offset:])
-	return offset
-}
-
-func (g *Getter) posOne(offset uint64) uint64 {
-	_, n := binary.Uvarint(g.posDict.data[offset:])
-	offset, _ = binary.Uvarint(g.posDict.data[offset+uint64(n):])
-	return offset
-}
-
-func (g *Getter) pattern(offset uint64) []byte {
-	l, n := binary.Uvarint(g.patternDict.data[offset:])
-	return g.patternDict.data[offset+uint64(n) : offset+uint64(n)+l]
-}
-
-func (g *Getter) pos(offset uint64) uint64 {
-	pos, _ := binary.Uvarint(g.posDict.data[offset:])
-	return pos
-}
-
 func (g *Getter) nextPos(clean bool) uint64 {
 	if clean {
 		g.mask = 0
 	}
-	offset := g.posDict.rootOffset
-	cutoff := g.posDict.cutoff
-	if offset < cutoff {
-		return g.pos(offset)
+	node := g.posDict
+	if node.zero == nil && node.one == nil {
+		return node.pos
 	}
 	b := g.b
 	mask := g.mask
 	dataP := g.dataP
-	for offset >= cutoff {
+	for node.zero != nil || node.one != nil {
 		if mask == 0 {
 			mask = 1
 			b = g.data[dataP]
 			dataP++
 		}
 		if b&mask == 0 {
-			offset = g.posZero(offset)
+			node = node.zero
 		} else {
-			offset = g.posOne(offset)
+			node = node.one
 		}
 		mask <<= 1
 	}
 	g.b = b
 	g.mask = mask
 	g.dataP = dataP
-	return g.pos(offset)
+	return node.pos
 }
 
 func (g *Getter) nextPattern() []byte {
-	offset := g.patternDict.rootOffset
-	cutoff := g.patternDict.cutoff
-	if offset < cutoff {
-		return g.pattern(offset)
+	node := g.patternDict
+	if node.zero == nil && node.one == nil {
+		return node.pattern
 	}
 	b := g.b
 	mask := g.mask
 	dataP := g.dataP
-	for offset >= cutoff {
+	for node.zero != nil || node.one != nil {
 		if mask == 0 {
 			mask = 1
 			b = g.data[dataP]
 			dataP++
 		}
 		if b&mask == 0 {
-			offset = g.zero(offset)
+			node = node.zero
 		} else {
-			offset = g.one(offset)
+			node = node.one
 		}
 		mask <<= 1
 	}
 	g.b = b
 	g.mask = mask
 	g.dataP = dataP
-	return g.pattern(offset)
+	return node.pattern
 }
 
 func (d *Decompressor) Count() int { return int(d.count) }
@@ -215,7 +213,7 @@ func (d *Decompressor) Count() int { return int(d.count) }
 // Getter is not thread-safe, but there can be multiple getters used simultaneously and concurrently
 // for the same decompressor
 func (d *Decompressor) MakeGetter() *Getter {
-	return &Getter{patternDict: &d.dict, posDict: &d.posDict, data: d.data[d.wordsStart:], uncovered: make([]int, 0, 128), fName: d.compressedFile}
+	return &Getter{patternDict: d.dict, posDict: d.posDict, data: d.data[d.wordsStart:], uncovered: make([]int, 0, 128), fName: d.compressedFile}
 }
 
 func (g *Getter) Reset(offset uint64) {
