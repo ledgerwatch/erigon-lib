c33d3437f7a5d78ccc10ae72bf43e85bf53a2b47
Alexey Sharp <alexeysharp@Alexeys-iMac.local>
2022-05-01 13:41:24 +0100

    More to test

--- state/domain_test.go
+++ state/domain_test.go
@@ -16,8 +16,41 @@
 
 package state
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/ledgerwatch/erigon-lib/kv"
+	"github.com/ledgerwatch/erigon-lib/kv/mdbx"
+	"github.com/ledgerwatch/log/v3"
+	"github.com/stretchr/testify/require"
+)
 
 func TestDomian(t *testing.T) {
+	path := t.TempDir()
+	logger := log.New()
+	valuesTable := "Values"
+	keysTable := "Keys"
+	historyTable := "History"
+	indexTable := "Index"
+	db := mdbx.NewMDBX(logger).Path(path).WithTablessCfg(func(defaultBuckets kv.TableCfg) kv.TableCfg {
+		return kv.TableCfg{
+			valuesTable:  kv.TableCfgItem{},
+			keysTable:    kv.TableCfgItem{Flags: kv.DupSort},
+			historyTable: kv.TableCfgItem{},
+			indexTable:   kv.TableCfgItem{Flags: kv.DupSort},
+		}
+	}).MustOpen()
+	defer db.Close()
+
+	d := NewDomain(16 /* aggregationStep */, "base" /* filenameBase */, valuesTable, keysTable, historyTable, indexTable)
+
+	tx, err := db.BeginRw(context.Background())
+	require.NoError(t, err)
+	defer tx.Rollback()
+
+	d.SetBlockNum(1)
+	d.SetTxNum(1)
+	d.SetTx(tx)
 
 }
