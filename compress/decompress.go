a5437b08e5433b53de25109ce1a114961b3b3239
alex.sharov <AskAlexSharov@gmail.com>
2022-05-17 12:00:09 +0700

    save

--- compress/decompress.go
+++ compress/decompress.go
@@ -552,6 +552,10 @@ func (g *Getter) MatchPrefix(prefix []byte) bool {
 	l := g.nextPos(true /* clean */)
 	l-- // because when create huffman tree we do ++ , because 0 is terminator
 	prefixLen := len(prefix)
+	if int(l) < prefixLen {
+		return false // if word shorter than prefix, no reason to compare
+	}
+
 	if l == 0 {
 		if g.dataBit > 0 {
 			g.dataP++
@@ -586,6 +590,7 @@ func (g *Getter) MatchPrefix(prefix []byte) bool {
 	postLoopPos := g.dataP
 	g.dataP, g.dataBit = savePos, 0
 	g.nextPos(true /* clean */) // Reset the state of huffman decoder
+
 	// Second pass - we check spaces not covered by the patterns
 	var lastUncovered int
 	prefixPos = 0
@@ -607,16 +612,24 @@ func (g *Getter) MatchPrefix(prefix []byte) bool {
 		}
 		lastUncovered = prefixPos + patternLen
 	}
-	if prefixLen > lastUncovered && int(l) > lastUncovered {
-		dif := l - uint64(lastUncovered)
-		var comparisonLen int
-		if prefixLen < int(l) {
-			comparisonLen = prefixLen - lastUncovered
-		} else {
-			comparisonLen = int(dif)
-		}
-		if !bytes.Equal(prefix[lastUncovered:lastUncovered+comparisonLen], g.data[postLoopPos:postLoopPos+uint64(comparisonLen)]) {
-			return false
+	if prefixLen > lastUncovered {
+		//if int(l) < prefixLen-lastUncovered {
+		//	return false // if word shorter than left part of prefix, no reason to compare
+		//}
+		if int(l) > lastUncovered {
+			dif := l - uint64(lastUncovered)
+			var comparisonLen int
+			if prefixLen < int(l) {
+				comparisonLen = prefixLen - lastUncovered
+			} else {
+				comparisonLen = int(dif)
+			}
+			if int(l) < prefixLen {
+				return false // if word shorter than prefix, no reason to compare
+			}
+			if !bytes.Equal(prefix[lastUncovered:lastUncovered+comparisonLen], g.data[postLoopPos:postLoopPos+uint64(comparisonLen)]) {
+				return false
+			}
 		}
 	}
 	return true
