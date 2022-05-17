4f011d6703daaae70aaa69ebf87075f25451b43b
alex.sharov <AskAlexSharov@gmail.com>
2022-05-17 12:11:58 +0700

    save

--- compress/decompress.go
+++ compress/decompress.go
@@ -483,7 +483,7 @@ func (g *Getter) Match(buf []byte) (bool, uint64) {
 	l := g.nextPos(true)
 	l-- // because when create huffman tree we do ++ , because 0 is terminator
 	lenBuf := len(buf)
-	if l == 0 {
+	if l == 0 || int(l) != lenBuf {
 		if g.dataBit > 0 {
 			g.dataP++
 			g.dataBit = 0
@@ -491,10 +491,7 @@ func (g *Getter) Match(buf []byte) (bool, uint64) {
 		if lenBuf != 0 {
 			g.dataP, g.dataBit = savePos, 0
 		}
-		return lenBuf == 0, g.dataP
-	}
-	if int(l) != lenBuf {
-		return false, g.dataP // if word shorter than prefix, no reason to compare
+		return lenBuf == int(l), g.dataP
 	}
 
 	var bufPos int
@@ -555,7 +552,7 @@ func (g *Getter) MatchPrefix(prefix []byte) bool {
 	l := g.nextPos(true /* clean */)
 	l-- // because when create huffman tree we do ++ , because 0 is terminator
 	prefixLen := len(prefix)
-	if l == 0 {
+	if l == 0 || int(l) < prefixLen {
 		if g.dataBit > 0 {
 			g.dataP++
 			g.dataBit = 0
@@ -563,10 +560,7 @@ func (g *Getter) MatchPrefix(prefix []byte) bool {
 		if prefixLen != 0 {
 			g.dataP, g.dataBit = savePos, 0
 		}
-		return prefixLen == 0
-	}
-	if int(l) < prefixLen {
-		return false // if word shorter than prefix, no reason to compare
+		return prefixLen == int(l)
 	}
 
 	var prefixPos int
