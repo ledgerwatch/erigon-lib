a704a227cd1adc12e65dd586c9190e1d56110d1b
alex.sharov <AskAlexSharov@gmail.com>
2022-10-10 20:35:56 +0700

    save

--- state/inverted_index.go
+++ state/inverted_index.go
@@ -724,7 +724,7 @@ func (ii *InvertedIndex) prune(txFrom, txTo, limit uint64) error {
 	defer func(t time.Time) {
 		fmt.Printf("inverted_index.go:683: %s, %s, %d\n", time.Since(t), ii.filenameBase, limit)
 	}(time.Now())
-	txTo = cmp.Min(txTo, txFrom+limit)
+	//txTo = cmp.Min(txTo, txFrom+limit)
 	keysCursor, err := ii.tx.RwCursorDupSort(ii.indexKeysTable)
 	if err != nil {
 		return fmt.Errorf("create %s keys cursor: %w", ii.filenameBase, err)
