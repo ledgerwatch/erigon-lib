894ce436f3d8fa9c7781a8a0bc67bbfdc0c066af
alex.sharov <AskAlexSharov@gmail.com>
2022-10-10 21:12:33 +0700

    save

--- state/inverted_index.go
+++ state/inverted_index.go
@@ -699,7 +699,7 @@ func (ii *InvertedIndex) warmup(txFrom, limit uint64, tx kv.Tx) error {
 	addrs := map[string]struct{}{}
 	k, v, err = keysCursor.Seek(txKey[:])
 	txFrom = binary.BigEndian.Uint64(k)
-	txTo = cmp.Min(txTo, txFrom+limit)
+	txTo := txFrom + limit
 	for ; err == nil && k != nil; k, v, err = keysCursor.Next() {
 		txNum := binary.BigEndian.Uint64(k)
 		if txNum >= txTo {
