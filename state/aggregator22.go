514916018919af24fb9b6b50294c02304a24be6d
alex.sharov <AskAlexSharov@gmail.com>
2022-10-13 12:23:57 +0700

    save

--- state/aggregator22.go
+++ state/aggregator22.go
@@ -834,7 +834,6 @@ func (a *Aggregator22) ReadyToFinishTx() bool {
 }
 
 func (a *Aggregator22) FinishTx() error {
-	return nil
 	if (a.txNum + 1) <= a.maxTxNum.Load()+2*a.aggregationStep { // Leave one step worth in the DB
 		return nil
 	}
