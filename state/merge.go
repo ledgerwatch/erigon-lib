dfb12c31a8c95ddad6f0f01a52e3e846c4e1a76a
alex.sharov <AskAlexSharov@gmail.com>
2022-10-14 21:40:12 +0700

    save

--- state/merge.go
+++ state/merge.go
@@ -341,6 +341,9 @@ func (d *Domain) mergeFiles(valuesFiles, indexFiles, historyFiles []*filesItem, 
 		return nil, nil, nil, err
 	}
 	if r.values {
+		if r.valuesStartTxNum == r.valuesEndTxNum {
+			return nil, indexIn, historyIn, nil
+		}
 		log.Info(fmt.Sprintf("[snapshots] merge: %s.%d-%d.kv", d.filenameBase, r.valuesStartTxNum/d.aggregationStep, r.valuesEndTxNum/d.aggregationStep))
 		for _, f := range valuesFiles {
 			defer f.decompressor.EnableMadvNormal().DisableReadAhead()
@@ -473,7 +476,7 @@ func (d *Domain) mergeFiles(valuesFiles, indexFiles, historyFiles []*filesItem, 
 
 func (ii *InvertedIndex) mergeFiles(files []*filesItem, startTxNum, endTxNum uint64, maxSpan uint64) (*filesItem, error) {
 	if startTxNum == endTxNum {
-		return nil, fmt.Errorf("%s: not allowed to create zero snapshot: %d-%d", ii.filenameBase, startTxNum, endTxNum)
+		return nil, nil
 	}
 	for _, h := range files {
 		defer h.decompressor.EnableMadvNormal().DisableReadAhead()
@@ -616,9 +619,8 @@ func (h *History) mergeFiles(indexFiles, historyFiles []*filesItem, r HistoryRan
 	}
 	if r.history {
 		if r.historyStartTxNum == r.historyEndTxNum {
-			return nil, nil, fmt.Errorf("%s: not allowed to create zero snapshot: %d-%d", h.filenameBase, r.historyStartTxNum, r.historyEndTxNum)
+			return indexIn, nil, nil
 		}
-
 		log.Info(fmt.Sprintf("[snapshots] merge: %s.%d-%d.v", h.filenameBase, r.historyStartTxNum/h.aggregationStep, r.historyEndTxNum/h.aggregationStep))
 		for _, f := range indexFiles {
 			defer f.decompressor.EnableMadvNormal().DisableReadAhead()
