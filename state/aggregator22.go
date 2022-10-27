d7d38531230de67521fe6ff523661b5e6cf6104c
alex.sharov <AskAlexSharov@gmail.com>
2022-10-27 11:13:44 +0700

    save

--- state/aggregator22.go
+++ state/aggregator22.go
@@ -559,39 +559,35 @@ func (a *Aggregator22) Warmup(txFrom, limit uint64) {
 		return
 	}
 	a.warmupWorking.Store(true)
-	initialFrom, initialTo := txFrom, txFrom+limit
-	for i := initialFrom; i < initialTo; i += limit / 4 {
-		go func(txFrom, limit uint64) {
-			defer a.warmupWorking.Store(false)
-			if err := a.db.View(context.Background(), func(tx kv.Tx) error {
-				if err := a.accounts.warmup(txFrom, limit, tx); err != nil {
-					return err
-				}
-				if err := a.storage.warmup(txFrom, limit, tx); err != nil {
-					return err
-				}
-				if err := a.code.warmup(txFrom, limit, tx); err != nil {
-					return err
-				}
-				if err := a.logAddrs.warmup(txFrom, limit, tx); err != nil {
-					return err
-				}
-				if err := a.logTopics.warmup(txFrom, limit, tx); err != nil {
-					return err
-				}
-				if err := a.tracesFrom.warmup(txFrom, limit, tx); err != nil {
-					return err
-				}
-				if err := a.tracesTo.warmup(txFrom, limit, tx); err != nil {
-					return err
-				}
-				return nil
-			}); err != nil {
-				log.Warn("[snapshots] prune warmup", "err", err)
+	go func() {
+		defer a.warmupWorking.Store(false)
+		if err := a.db.View(context.Background(), func(tx kv.Tx) error {
+			if err := a.accounts.warmup(txFrom, limit, tx); err != nil {
+				return err
 			}
-		}(i, limit/10)
-	}
-
+			if err := a.storage.warmup(txFrom, limit, tx); err != nil {
+				return err
+			}
+			if err := a.code.warmup(txFrom, limit, tx); err != nil {
+				return err
+			}
+			if err := a.logAddrs.warmup(txFrom, limit, tx); err != nil {
+				return err
+			}
+			if err := a.logTopics.warmup(txFrom, limit, tx); err != nil {
+				return err
+			}
+			if err := a.tracesFrom.warmup(txFrom, limit, tx); err != nil {
+				return err
+			}
+			if err := a.tracesTo.warmup(txFrom, limit, tx); err != nil {
+				return err
+			}
+			return nil
+		}); err != nil {
+			log.Warn("[snapshots] prune warmup", "err", err)
+		}
+	}()
 }
 
 // StartWrites - pattern: `defer agg.StartWrites().FinishWrites()`
