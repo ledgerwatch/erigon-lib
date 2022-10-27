953d272d29df372e28395b2c6c7c8e89ed9e6484
alex.sharov <AskAlexSharov@gmail.com>
2022-10-27 09:18:51 +0700

    save

--- state/aggregator22.go
+++ state/aggregator22.go
@@ -247,8 +247,6 @@ func (a *Aggregator22) collate(step uint64, txFrom, txTo uint64, db kv.RoDB) (Ag
 	defer func(t time.Time) { log.Info(fmt.Sprintf("aggregator22.go:247: collate %s\n", time.Since(t))) }(time.Now())
 	log.Info("collate start2", "from", txFrom, "to", txTo)
 	ctx := context.TODO()
-	roTx, _ := db.BeginRo(context.Background())
-	defer roTx.Rollback()
 	var ac Agg22Collation
 	closeColl := true
 	defer func() {
