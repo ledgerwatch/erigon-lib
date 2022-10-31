43ebfc144c7b64863f0454b431dac556c2be279a
alex.sharov <AskAlexSharov@gmail.com>
2022-10-31 20:44:33 +0700

    save

--- state/aggregator22.go
+++ state/aggregator22.go
@@ -619,6 +619,11 @@ func (a *Aggregator22) CanPruneFrom(tx kv.Tx) uint64 {
 }
 func (a *Aggregator22) Prune(ctx context.Context, limit uint64) error {
 	a.Warmup(0, cmp.Max(a.aggregationStep, limit)) // warmup is asyn and moving faster than data deletion
+	defer func(t time.Time) {
+		if time.Since(t) > 50*time.Millisecond {
+			log.Debug(fmt.Sprintf("prune took: %s\n", time.Since(t)))
+		}
+	}(time.Now())
 	return a.prune(ctx, 0, a.maxTxNum.Load(), limit)
 }
 
