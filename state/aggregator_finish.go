20677bf7c150d83ab56e91b8df9c83413965f638
awskii <artem.tsskiy@gmail.com>
2022-11-10 12:11:13 +0000

    fix

--- state/aggregator_finish.go
+++ state/aggregator_finish.go
@@ -142,6 +142,27 @@ func (a *Aggregator) collator(ctx context.Context, step uint64) error {
 	errCh := make(chan error, 8)
 	var wg sync.WaitGroup
 
+	for _, d := range []*InvertedIndex{a.logTopics, a.logAddrs, a.tracesFrom, a.tracesTo} {
+		collation, err := d.collate(ctx, step*a.aggregationStep, (step+1)*a.aggregationStep, a.rwTx, logEvery)
+		if err != nil {
+			return err
+		}
+
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, d *InvertedIndex, collation map[string]*roaring64.Bitmap) {
+			defer wg.Done()
+
+			sf, err := d.buildFiles(ctx, step, collation)
+			if err != nil {
+				errCh <- err
+				sf.Close()
+				return
+			}
+			d.integrateFiles(sf, step*a.aggregationStep, (step+1)*a.aggregationStep)
+			sf.Close()
+		}(&wg, d, collation)
+	}
+
 	for _, d := range []*Domain{a.accounts, a.storage, a.code, a.commitment.Domain} {
 		collation, err := d.collate(ctx, step, step*a.aggregationStep, (step+1)*a.aggregationStep, a.rwTx, logEvery)
 		if err != nil {
@@ -165,27 +186,6 @@ func (a *Aggregator) collator(ctx context.Context, step uint64) error {
 		}(&wg, d, collation)
 	}
 
-	for _, d := range []*InvertedIndex{a.logTopics, a.logAddrs, a.tracesFrom, a.tracesTo} {
-		collation, err := d.collate(ctx, step*a.aggregationStep, (step+1)*a.aggregationStep, a.rwTx, logEvery)
-		if err != nil {
-			return err
-		}
-
-		wg.Add(1)
-		go func(wg *sync.WaitGroup, d *InvertedIndex, collation map[string]*roaring64.Bitmap) {
-			defer wg.Done()
-
-			sf, err := d.buildFiles(ctx, step, collation)
-			if err != nil {
-				errCh <- err
-				sf.Close()
-				return
-			}
-			d.integrateFiles(sf, step*a.aggregationStep, (step+1)*a.aggregationStep)
-			sf.Close()
-		}(&wg, d, collation)
-	}
-
 	go func() {
 		wg.Wait()
 		close(errCh)
@@ -195,15 +195,15 @@ func (a *Aggregator) collator(ctx context.Context, step uint64) error {
 		log.Warn("build domain files failed", "err", err)
 	}
 
-	for _, d := range []*Domain{a.accounts, a.storage, a.code, a.commitment.Domain} {
-		err := d.prune(ctx, step, step*a.aggregationStep, (step+1)*a.aggregationStep, math.MaxUint64, logEvery)
+	for _, d := range []*InvertedIndex{a.logTopics, a.logAddrs, a.tracesFrom, a.tracesTo} {
+		err := d.prune(ctx, step*a.aggregationStep, (step+1)*a.aggregationStep, math.MaxUint64, logEvery)
 		if err != nil {
 			return err
 		}
 	}
 
-	for _, d := range []*InvertedIndex{a.logTopics, a.logAddrs, a.tracesFrom, a.tracesTo} {
-		err := d.prune(ctx, step*a.aggregationStep, (step+1)*a.aggregationStep, math.MaxUint64, logEvery)
+	for _, d := range []*Domain{a.accounts, a.storage, a.code, a.commitment.Domain} {
+		err := d.prune(ctx, step, step*a.aggregationStep, (step+1)*a.aggregationStep, math.MaxUint64, logEvery)
 		if err != nil {
 			return err
 		}
@@ -214,16 +214,57 @@ func (a *Aggregator) collator(ctx context.Context, step uint64) error {
 	for r := a.findMergeRange(maxEndTxNum, maxSpan); r.any(); r = a.findMergeRange(maxEndTxNum, maxSpan) {
 		outs := a.staticFilesInRange(r)
 		in, err := a.mergeFiles(ctx, outs, r, 1)
-		outs.Close()
 		if err != nil {
+			outs.Close()
 			return err
 		}
 		a.integrateMergedFiles(outs, in)
 		in.Close()
 
 		if err = a.deleteFiles(outs); err != nil {
+			outs.Close()
 			return err
 		}
+		outs.Close()
 	}
+	return nil
+}
+
+type mergeArtifacts struct {
+	sfr staticFilesInRange
+	mf  mergedDomainFiles
+}
+
+func (d *Domain) mergeRangesUpTo(ctx context.Context, maxTxNum, maxSpan uint64, workers int, output chan mergedDomainFiles) (err error) {
+	//closeAll := true
+	for rng := d.findMergeRange(maxSpan, maxTxNum); rng.any(); rng = d.findMergeRange(maxTxNum, maxSpan) {
+		var sfr staticFilesInRange
+		sfr.valuesFiles, sfr.indexFiles, sfr.historyFiles, sfr.startJ = d.staticFilesInRange(rng)
+		//defer func() {
+		//	if closeAll {
+		//		sfr.Close()
+		//	}
+		//}()
+
+		var mf mergedDomainFiles
+		if mf.values, mf.index, mf.history, err = d.mergeFiles(ctx, sfr.valuesFiles, sfr.indexFiles, sfr.historyFiles, rng, workers); err != nil {
+			return err
+		}
+		//defer func() {
+		//	if closeAll {
+		//		mf.Close()
+		//	}
+		//}()
+
+		//defer func(t time.Time) { log.Info("[snapshots] merge", "took", time.Since(t)) }(time.Now())
+		d.integrateMergedFiles(sfr.valuesFiles, sfr.indexFiles, sfr.historyFiles, mf.values, mf.index, mf.history)
+
+		if err := d.deleteFiles(sfr.valuesFiles, sfr.indexFiles, sfr.historyFiles); err != nil {
+			return err
+		}
+
+		log.Info(fmt.Sprintf("findMergeRange(%d, %d)=%+v\n", maxTxNum, maxSpan, rng))
+	}
+	//closeAll = false
 	return nil
 }
