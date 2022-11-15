a4fab30dd7ac609261021080de05f6ceeb514c73
Manav Darji <manavdarji.india@gmail.com>
2022-11-15 18:07:40 +0530

    txpool: honour nonce while sorting txs

--- txpool/pool.go
+++ txpool/pool.go
@@ -2231,16 +2231,27 @@ func (mt *metaTx) better(than *metaTx, pendingBaseFee uint256.Int) bool {
 		if effectiveTip.Cmp(&thanEffectiveTip) != 0 {
 			return effectiveTip.Cmp(&thanEffectiveTip) > 0
 		}
+		// Only compare nonces and balances for same sender
+		if mt.Tx.SenderID == than.Tx.SenderID {
+			if mt.nonceDistance != than.nonceDistance {
+				return mt.nonceDistance > than.nonceDistance
+			}
+			if mt.cumulativeBalanceDistance != than.cumulativeBalanceDistance {
+				return mt.cumulativeBalanceDistance > than.cumulativeBalanceDistance
+			}
+		}
 	case BaseFeeSubPool:
 		if mt.minFeeCap.Cmp(&than.minFeeCap) != 0 {
 			return mt.minFeeCap.Cmp(&than.minFeeCap) > 0
 		}
 	case QueuedSubPool:
-		if mt.nonceDistance != than.nonceDistance {
-			return mt.nonceDistance < than.nonceDistance
-		}
-		if mt.cumulativeBalanceDistance != than.cumulativeBalanceDistance {
-			return mt.cumulativeBalanceDistance < than.cumulativeBalanceDistance
+		if mt.Tx.SenderID == than.Tx.SenderID {
+			if mt.nonceDistance != than.nonceDistance {
+				return mt.nonceDistance < than.nonceDistance
+			}
+			if mt.cumulativeBalanceDistance != than.cumulativeBalanceDistance {
+				return mt.cumulativeBalanceDistance < than.cumulativeBalanceDistance
+			}
 		}
 	}
 	return mt.timestamp < than.timestamp
@@ -2264,18 +2275,23 @@ func (mt *metaTx) worse(than *metaTx, pendingBaseFee uint256.Int) bool {
 		if mt.minFeeCap != than.minFeeCap {
 			return mt.minFeeCap.Cmp(&than.minFeeCap) < 0
 		}
-		if mt.nonceDistance != than.nonceDistance {
-			return mt.nonceDistance > than.nonceDistance
-		}
-		if mt.cumulativeBalanceDistance != than.cumulativeBalanceDistance {
-			return mt.cumulativeBalanceDistance > than.cumulativeBalanceDistance
+		// Only compare nonces and balances for same sender
+		if mt.Tx.SenderID == than.Tx.SenderID {
+			if mt.nonceDistance != than.nonceDistance {
+				return mt.nonceDistance > than.nonceDistance
+			}
+			if mt.cumulativeBalanceDistance != than.cumulativeBalanceDistance {
+				return mt.cumulativeBalanceDistance > than.cumulativeBalanceDistance
+			}
 		}
 	case BaseFeeSubPool, QueuedSubPool:
-		if mt.nonceDistance != than.nonceDistance {
-			return mt.nonceDistance > than.nonceDistance
-		}
-		if mt.cumulativeBalanceDistance != than.cumulativeBalanceDistance {
-			return mt.cumulativeBalanceDistance > than.cumulativeBalanceDistance
+		if mt.Tx.SenderID == than.Tx.SenderID {
+			if mt.nonceDistance != than.nonceDistance {
+				return mt.nonceDistance > than.nonceDistance
+			}
+			if mt.cumulativeBalanceDistance != than.cumulativeBalanceDistance {
+				return mt.cumulativeBalanceDistance > than.cumulativeBalanceDistance
+			}
 		}
 	}
 	return mt.timestamp > than.timestamp
