abf9da4d95d29887ba132f8a52962647e5fea89a
alex.sharov <AskAlexSharov@gmail.com>
2023-01-02 10:59:14 +0700

    save

--- kv/bitmapdb/fixed_size.go
+++ kv/bitmapdb/fixed_size.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 Erigon contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package bitmapdb
 
 import (
@@ -12,11 +28,9 @@ import (
 
 	"github.com/RoaringBitmap/roaring/roaring64"
 	mmap2 "github.com/edsrzf/mmap-go"
-	"github.com/kelindar/bitmap"
 )
 
 type FixedSizeBitmaps struct {
-	bitmap.Bitmap
 	f         *os.File
 	indexFile string
 	data      []uint64
