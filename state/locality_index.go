70c75c4c8c94d6a7979a6b45e281116b6f4ac77b
alex.sharov <AskAlexSharov@gmail.com>
2023-01-06 10:11:35 +0700

    save

--- state/locality_index.go
+++ state/locality_index.go
@@ -412,7 +412,6 @@ func (ic *InvertedIndexContext) iterateKeysLocality(uptoTxNum uint64) *LocalityI
 			heap.Push(&si.h, heapItem)
 		}
 		si.totalOffsets += uint64(item.getter.Size())
-		log.Warn("files", "name", ic.localityIndex.filenameBase, "item", fmt.Sprintf("%d-%d", item.startTxNum/ic.ii.aggregationStep, item.endTxNum/ic.ii.aggregationStep))
 		si.filesAmount++
 		return true
 	})
