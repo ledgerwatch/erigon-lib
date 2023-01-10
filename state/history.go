cdd2571f069f6a908d7bd326fc9f4958c674306e
alex.sharov <AskAlexSharov@gmail.com>
2023-01-10 13:11:00 +0700

    save

--- state/history.go
+++ state/history.go
@@ -148,9 +148,6 @@ Loop:
 			var superSet *filesItem
 			h.files.Ascend(func(it *filesItem) bool {
 				if it.isSubsetOf(item) {
-					a := fmt.Sprintf("%s.%d-%d.v", h.filenameBase, it.startTxNum/h.aggregationStep, it.endTxNum/h.aggregationStep)
-					b := fmt.Sprintf("%s.%d-%d.v", h.filenameBase, item.startTxNum/h.aggregationStep, item.endTxNum/h.aggregationStep)
-					log.Debug(fmt.Sprintf("[snapshots] skip %s it's subset of %s", a, b))
 					subSets = append(subSets, it)
 				} else if item.isSubsetOf(it) {
 					superSet = it
@@ -1069,7 +1066,7 @@ func (h *History) MakeContext() *HistoryContext {
 	var hc = HistoryContext{
 		h:          h,
 		indexFiles: btree.NewG[ctxItem](32, ctxItemLess),
-		trace:      true,
+		trace:      false,
 	}
 	h.InvertedIndex.files.Ascend(func(item *filesItem) bool {
 		if item.index == nil {
