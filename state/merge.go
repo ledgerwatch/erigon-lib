8d1e5ef2abc95bd29b580663e2b5313056238546
alex.sharov <AskAlexSharov@gmail.com>
2023-01-30 17:41:33 +0700

    save

--- state/merge.go
+++ state/merge.go
@@ -977,8 +977,6 @@ func (ii *InvertedIndex) integrateMergedFiles(outs []*filesItem, in *filesItem) 
 		ii.files.ReplaceOrInsert(in)
 	}
 	for _, out := range outs {
-		fmt.Printf("remove from trie: %s\n", out.decompressor.FilePath())
-		fmt.Printf("remove from trie: %s\n", out.index.FilePath())
 		if out == nil {
 			panic("must not happen: " + ii.filenameBase)
 		}
@@ -1023,8 +1021,6 @@ func (d *Domain) deleteFiles(valuesOuts, indexOuts, historyOuts []*filesItem) er
 
 func (ii *InvertedIndex) deleteFiles(outs []*filesItem) error {
 	for _, out := range outs {
-		fmt.Printf("mark canDelete: %s\n", out.decompressor.FilePath())
-		fmt.Printf("mark canDelete: %s\n", out.index.FilePath())
 		out.canDelete.Store(true)
 
 		//datPath := filepath.Join(ii.dir, fmt.Sprintf("%s.%d-%d.ef", ii.filenameBase, out.startTxNum/ii.aggregationStep, out.endTxNum/ii.aggregationStep))
