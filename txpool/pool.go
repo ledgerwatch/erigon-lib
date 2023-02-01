1198d8fb5f0a1a20041effa96ebf43f632d99bfb
yperbasis <andrey.ashikhmin@gmail.com>
2023-02-01 14:00:54 +0100

    Fix index out of range in TxPool.best

--- txpool/pool.go
+++ txpool/pool.go
@@ -612,15 +612,17 @@ func (p *TxPool) IsLocal(idHash []byte) bool {
 func (p *TxPool) AddNewGoodPeer(peerID types.PeerID) { p.recentlyConnectedPeers.AddPeer(peerID) }
 func (p *TxPool) Started() bool                      { return p.started.Load() }
 
-func (p *TxPool) best(n uint16, txs *types.TxsRlp, tx kv.Tx, onTopOf, availableGas uint64, toSkip mapset.Set[[32]byte]) (bool, int, error) {
+func (p *TxPool) best(n uint, txs *types.TxsRlp, tx kv.Tx, onTopOf, availableGas uint64, toSkip mapset.Set[[32]byte]) (bool, int, error) {
 	// First wait for the corresponding block to arrive
 	if p.lastSeenBlock.Load() < onTopOf {
 		return false, 0, nil // Too early
 	}
 
 	isShanghai := p.isShanghai()
+	best := p.pending.best
 
-	txs.Resize(uint(cmp.Min(int(n), len(p.pending.best.ms))))
+	n = uint(cmp.Min(int(n), len(best.ms)))
+	txs.Resize(n)
 	var toRemove []*metaTx
 	count := 0
 
@@ -628,7 +630,6 @@ func (p *TxPool) best(n uint16, txs *types.TxsRlp, tx kv.Tx, onTopOf, availableG
 		p.lock.Lock()
 		defer p.lock.Unlock()
 
-		best := p.pending.best
 		for i := 0; count < int(n) && i < len(best.ms); i++ {
 			// if we wouldn't have enough gas for a standard transaction then quit out early
 			if availableGas < fixedgas.TxGas {
@@ -696,12 +697,12 @@ func (p *TxPool) ResetYieldedStatus() {
 }
 
 func (p *TxPool) YieldBest(n uint16, txs *types.TxsRlp, tx kv.Tx, onTopOf, availableGas uint64, toSkip mapset.Set[[32]byte]) (bool, int, error) {
-	return p.best(n, txs, tx, onTopOf, availableGas, toSkip)
+	return p.best(uint(n), txs, tx, onTopOf, availableGas, toSkip)
 }
 
 func (p *TxPool) PeekBest(n uint16, txs *types.TxsRlp, tx kv.Tx, onTopOf, availableGas uint64) (bool, error) {
 	set := mapset.NewSet[[32]byte]()
-	onTime, _, err := p.best(n, txs, tx, onTopOf, availableGas, set)
+	onTime, _, err := p.best(uint(n), txs, tx, onTopOf, availableGas, set)
 	return onTime, err
 }
 
