40c987bc1dded5bf04eb777bcbf9cd3c43e5187a
alex.sharov <AskAlexSharov@gmail.com>
2023-05-03 10:27:10 +0700

    save

--- commitment/hex_patricia_hashed.go
+++ commitment/hex_patricia_hashed.go
@@ -722,7 +722,8 @@ func (hph *HexPatriciaHashed) computeCellHash(cell *Cell, depth int, buf []byte)
 		var valBuf [128]byte
 		valLen := cell.accountForHashing(valBuf[:], storageRootHash)
 		if hph.trace {
-			fmt.Printf("accountLeafHashWithKey for [%x]=>[%x], storageRootHash=%x, cell.hl=%d\n", hph.hashAuxBuffer[:65-depth], valBuf[:valLen], storageRootHash, cell.hl)
+			//fmt.Printf("accountLeafHashWithKey for [%x]=>[%x]\n", hph.hashAuxBuffer[:65-depth], valBuf[:valLen])
+			fmt.Printf("accountLeafHashWithKey for [%x]=>[%x]\n", cell.downHashedKey[:65-depth], valBuf[:valLen])
 		}
 		return hph.accountLeafHashWithKey(buf, cell.downHashedKey[:65-depth], rlp.RlpEncodedBytes(valBuf[:valLen]))
 	}
