b3ab3d56423ce8902e5ffcb89fed68bd9f7acb14
alex.sharov <AskAlexSharov@gmail.com>
2023-05-08 10:13:03 +0700

    leak detector

--- common/dbg/log_panic.go
+++ common/dbg/log_panic.go
@@ -17,88 +17,10 @@
 package dbg
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
-	"sync"
-	"sync/atomic"
-	"time"
-
 	stack2 "github.com/go-stack/stack"
-	"github.com/ledgerwatch/log/v3"
 )
 
 // Stack returns stack-trace in logger-friendly compact formatting
 func Stack() string {
 	return stack2.Trace().TrimBelow(stack2.Caller(1)).String()
-}
-
-type LeakDetector struct {
-	enabled       bool
-	doTraceTxs    bool
-	traceTxs      map[uint64]LeakDetectorItem
-	autoIncrement atomic.Uint64
-	lock          sync.Mutex
-}
-
-type LeakDetectorItem struct {
-	stack   string
-	started time.Time
-}
-
-func NewLeakDetector(name string, enabled bool) *LeakDetector {
-	d := &LeakDetector{enabled: enabled}
-	if enabled {
-		go func() {
-			logEvery := time.NewTicker(30 * time.Second)
-			defer logEvery.Stop()
-
-			for {
-				select {
-				case <-logEvery.C:
-					if list := d.slowList(); len(list) > 0 {
-						log.Info(fmt.Sprintf("[dbg.%s]", name), "slow", strings.Join(d.slowList(), ","))
-					}
-				}
-			}
-		}()
-	}
-	return d
-}
-
-func (d *LeakDetector) slowList() (res []string) {
-	if d == nil || !d.enabled {
-		return res
-	}
-	d.lock.Lock()
-	defer d.lock.Unlock()
-	for key, value := range d.traceTxs {
-		if time.Since(value.started) > time.Minute {
-			res = append(res, strconv.Itoa(int(key))+": "+value.stack)
-		}
-	}
-	return res
-}
-
-func (d *LeakDetector) Del(id uint64) {
-	if d == nil || !d.enabled {
-		return
-	}
-	d.lock.Lock()
-	defer d.lock.Unlock()
-	delete(d.traceTxs, id)
-}
-func (d *LeakDetector) Add() uint64 {
-	if d == nil || !d.enabled {
-		return 0
-	}
-	ac := LeakDetectorItem{
-		stack:   Stack(),
-		started: time.Now(),
-	}
-	id := d.autoIncrement.Add(1)
-	d.lock.Lock()
-	defer d.lock.Unlock()
-	d.traceTxs[id] = ac
-	return 0
 }
