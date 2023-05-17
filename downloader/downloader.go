d7b36479613ab71ddf49357e5cd92eaa49003ed9
alex.sharov <AskAlexSharov@gmail.com>
2023-05-17 10:30:56 +0700

    save

--- downloader/downloader.go
+++ downloader/downloader.go
@@ -22,7 +22,6 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
-	"runtime"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -346,7 +345,7 @@ func (d *Downloader) VerifyData(ctx context.Context) error {
 
 	j := &atomic.Uint64{}
 	g, ctx := errgroup.WithContext(ctx)
-	g.SetLimit(runtime.GOMAXPROCS(-1) * 2) // torrent lib internally limiting amount of hashers per file
+	g.SetLimit(16) // torrent lib internally limiting amount of hashers per file
 
 	for _, t := range d.torrentClient.Torrents() {
 		t := t
