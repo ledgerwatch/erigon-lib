0bc131a8bcabdf0c23760cbce8943a27b8f6683b
alex.sharov <AskAlexSharov@gmail.com>
2023-05-17 10:42:25 +0700

    save

--- downloader/downloader.go
+++ downloader/downloader.go
@@ -365,6 +365,7 @@ func (d *Downloader) VerifyData(ctx context.Context) error {
 		for {
 			select {
 			case <-ctx.Done():
+				log.Warn("[dbg} verify", "err", ctx.Err())
 				return
 			case <-logEvery.C:
 				log.Info("[snapshots] Verify", "progress", fmt.Sprintf("%.2f%%", 100*float64(j.Load())/float64(total)))
