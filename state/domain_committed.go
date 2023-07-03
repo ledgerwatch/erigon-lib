8bcf107bf09260e3775e938cbbc78c307909988e
alex.sharov <AskAlexSharov@gmail.com>
2023-07-03 12:15:38 +0700

    save

--- state/domain_committed.go
+++ state/domain_committed.go
@@ -688,8 +688,8 @@ func (d *DomainCommitted) ComputeCommitment(trace bool) (rootHash []byte, branch
 	touchedKeys, hashedKeys, updates := d.updates.List(true)
 	d.comKeys = uint64(len(touchedKeys))
 
-	for _, key := range touchedKeys {
-		fmt.Printf("plain: %x\n", key)
+	for i, key := range touchedKeys {
+		fmt.Printf("plain: %x, %x\n", key, updates[i].CodeValue)
 	}
 	if len(touchedKeys) == 0 {
 		rootHash, err = d.patriciaTrie.RootHash()
