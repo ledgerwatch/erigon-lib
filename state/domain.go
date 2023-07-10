638e7199bd9dc0b1e8658396cde6a7ad539e0470
alex.sharov <AskAlexSharov@gmail.com>
2023-07-10 09:40:56 +0700

    save

--- state/domain.go
+++ state/domain.go
@@ -1436,9 +1436,8 @@ func (dc *DomainContext) getLatestFromFiles(filekey []byte) (v []byte, found boo
 	dc.d.stats.FilesQueries.Add(1)
 
 	if hits[4]%10_000 == 0 {
-		fmt.Printf("hits: %d, %d\n", len(dc.files), hits[:len(dc.files)])
 		for i := len(dc.files) - 1; i >= 0; i-- {
-			fmt.Printf("file: %s\n", dc.files[i].src.decompressor.FileName())
+			fmt.Printf("file: %s, %dk\n", dc.files[i].src.decompressor.FileName(), hits[i]/1_000)
 		}
 	}
 
