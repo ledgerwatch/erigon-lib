4cdb4d360eadb0014decefc0dfbfbb6b41b38e9f
a <a@tuxpa.in>
2023-08-31 20:04:02 -0500

    more

--- rlp/decoder.go
+++ rlp/decoder.go
@@ -1,126 +1,183 @@
 package rlp
 
 import (
+	"bytes"
 	"fmt"
 	"reflect"
 )
 
-func unmarshal(data []byte, val any) error {
+func Unmarshal(data []byte, val any) error {
+	buf := bytes.NewBuffer(data)
+	return unmarshal(buf, val)
+
+}
+func unmarshal(buf *bytes.Buffer, val any) error {
 	rv := reflect.ValueOf(val)
 	if rv.Kind() != reflect.Pointer || rv.IsNil() {
 		return fmt.Errorf("%w: v must be ptr", ErrDecode)
 	}
-
 	v := rv.Elem()
-
-	// read the first byte
-	if len(data) == 0 {
-		return ErrUnexpectedEOF
+	err := reflectAny(buf, v, rv)
+	if err != nil {
+		return fmt.Errorf("%w: %w", ErrDecode, err)
 	}
+	return nil
+}
 
+func nextFull(dat *bytes.Buffer, size int) ([]byte, error) {
+	d := dat.Next(size)
+	if len(d) != size {
+		return nil, ErrUnexpectedEOF
+	}
+	return d, nil
+}
+
+// BeInt parses Big Endian representation of an integer from given payload at given position
+func decodeBeInt(w *bytes.Buffer, length int) (int, error) {
+	var r int
+	dat, err := nextFull(w, length)
+	if err != nil {
+		return 0, ErrUnexpectedEOF
+	}
+	if length > 0 && dat[0] == 0 {
+		return 0, fmt.Errorf("%w: integer encoding for RLP must not have leading zeros: %x", ErrParse, dat)
+	}
+	for _, b := range dat[0:length] {
+		r = (r << 8) | int(b)
+	}
+	return r, nil
+}
+
+func reflectAny(w *bytes.Buffer, v reflect.Value, rv reflect.Value) error {
 	// figure out what we are reading
-	token := identifyToken(data[0])
-
+	prefix, err := w.ReadByte()
+	if err != nil {
+		return err
+	}
+	token := identifyToken(prefix)
 	// switch
 	switch token {
 	case TokenDecimal:
 		// in this case, the value is just the byte itself
 		switch v.Kind() {
 		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			v.SetInt(int64(data[0]))
+			v.SetInt(int64(prefix))
 		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-			v.SetUint(uint64(data[0]))
+			v.SetUint(uint64(prefix))
+		case reflect.Invalid:
+			// do nothing
 		default:
 			return fmt.Errorf("%w: decimal must be unmarshal into integer type", ErrDecode)
 		}
 	case TokenShortString:
-		sz := int(token.Diff(data[0]))
-		if len(data) <= 1+sz {
-			return ErrUnexpectedEOF
+		sz := int(token.Diff(prefix))
+		str, err := nextFull(w, sz)
+		if err != nil {
+			return err
 		}
-		dat := data[1 : 1+sz]
-		return reflectString(dat, v, rv)
+		return putString(str, v, rv)
 	case TokenLongString:
-		lenSz := int(token.Diff(data[0]))
-		if len(data) <= 1+lenSz {
-			return ErrUnexpectedEOF
-		}
-		sz, err := BeInt(data, 1, lenSz)
+		lenSz := int(token.Diff(prefix))
+		sz, err := decodeBeInt(w, lenSz)
 		if err != nil {
 			return err
 		}
-		if len(data) <= 1+sz {
-			return ErrUnexpectedEOF
+		str, err := nextFull(w, sz)
+		if err != nil {
+			return err
 		}
-		dat := data[1+lenSz : 1+sz+lenSz]
-		return reflectString(dat, v, rv)
+		return putString(str, v, rv)
 	case TokenShortList:
-		sz := int(token.Diff(data[0]))
-		if len(data) <= 1+sz {
-			return ErrUnexpectedEOF
-		}
-		dat := data[1 : 1+sz]
-		return reflectList(dat, v, rv)
-	case TokenLongList:
-		lenSz := int(token.Diff(data[0]))
-		if len(data) <= 1+lenSz {
-			return ErrUnexpectedEOF
-		}
-		sz, err := BeInt(data, 1, lenSz)
+		sz := int(token.Diff(prefix))
+		buf, err := nextFull(w, sz)
 		if err != nil {
 			return err
 		}
-		if len(data) <= 1+sz {
-			return ErrUnexpectedEOF
+		return reflectList(bytes.NewBuffer(buf), v, rv)
+	case TokenLongList:
+		lenSz := int(token.Diff(prefix))
+		sz, err := decodeBeInt(w, lenSz)
+		if err != nil {
+			return err
 		}
-		dat := data[1+lenSz : 1+sz+lenSz]
-		return reflectList(dat, v, rv)
+		buf, err := nextFull(w, sz)
+		if err != nil {
+			return err
+		}
+		return reflectList(bytes.NewBuffer(buf), v, rv)
 	case TokenUnknown:
 		return fmt.Errorf("%w: unknown token", ErrDecode)
 	}
 	return nil
 }
 
-func reflectString(dat []byte, v reflect.Value, rv reflect.Value) error {
+func putString(w []byte, v reflect.Value, rv reflect.Value) error {
 	switch v.Kind() {
 	case reflect.String:
-		v.SetString(string(dat))
+		v.SetString(string(w))
 	case reflect.Slice:
 		if v.Type().Elem().Kind() != reflect.Uint8 {
 			return fmt.Errorf("%w: need to use uint8 as underlying if want slice output from longstring", ErrDecode)
 		}
-		v.SetBytes(dat)
+		v.SetBytes(w)
 	case reflect.Array:
 		if v.Type().Elem().Kind() != reflect.Uint8 {
 			return fmt.Errorf("%w: need to use uint8 as underlying if want array output from longstring", ErrDecode)
 		}
-		reflect.Copy(v, reflect.ValueOf(dat))
+		reflect.Copy(v, reflect.ValueOf(w))
+	case reflect.Invalid:
+		// do nothing
+		return nil
 	}
-
 	return nil
 }
 
-func reflectList(dat []byte, v reflect.Value, rv reflect.Value) error {
+func reflectList(w *bytes.Buffer, v reflect.Value, rv reflect.Value) error {
 	switch v.Kind() {
+	case reflect.Invalid:
+		// do nothing
+		return nil
 	case reflect.Map:
-		// TODO: read two elements.
 		rv1 := reflect.New(v.Type().Key())
 		v1 := rv1.Elem()
-		err := reflectString(dat, v1, rv1)
+		err := reflectAny(w, v1, rv1)
 		if err != nil {
 			return err
 		}
-		//TODO: need to advance dat cursor - create helper class
 		rv2 := reflect.New(v.Type().Elem())
-		v2 := rv1.Elem()
-		err = reflectString(dat, v2, rv2)
+		v2 := rv2.Elem()
+		err = reflectAny(w, v2, rv2)
 		if err != nil {
 			return err
 		}
-	case reflect.Array:
-		// TODO: read up to N elements
-	case reflect.Slice:
-		// TODO: read all elements into slice, creating more if needed
+		v.SetMapIndex(rv1, rv2)
+	case reflect.Array, reflect.Slice:
+		idx := 0
+		for {
+			if idx >= v.Cap() {
+				v.Grow(1)
+			}
+			if idx >= v.Len() {
+				v.SetLen(idx + 1)
+			}
+			if idx < v.Len() {
+				// Decode into element.
+				rv1 := v.Index(idx)
+				v1 := rv1.Elem()
+				err := reflectAny(w, v1, rv1)
+				if err != nil {
+					return err
+				}
+			} else {
+				// Ran out of fixed array: skip.
+				rv1 := reflect.Value{}
+				err := reflectAny(w, rv1, rv1)
+				if err != nil {
+					return err
+				}
+			}
+			idx++
+		}
 	}
 	return nil
 }
